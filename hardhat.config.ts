642606f954690785a61cec4622a60f33aae89dad
Germán Martínez <germaga92@gmail.com>
2021-03-02 11:39:25 +0100

    Handle env variables in hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,37 @@
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import dotenv from 'dotenv'
 import 'hardhat-deploy'
 import 'hardhat-typechain'
-import { HardhatUserConfig } from 'hardhat/types'
+import { HardhatUserConfig, HttpNetworkUserConfig } from 'hardhat/types'
+import yargs from 'yargs'
+
+const argv = yargs
+  .option('network', {
+    type: 'string',
+    default: 'hardhat',
+  })
+  .help(false)
+  .version(false).argv
+
+dotenv.config()
+const { INFURA_KEY, MNEMONIC, PK } = process.env
+const DEFAULT_MNEMONIC = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
+
+const sharedNetworkConfig: HttpNetworkUserConfig = {}
+if (PK) {
+  sharedNetworkConfig.accounts = [PK];
+} else {
+  sharedNetworkConfig.accounts = {
+    mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+  }
+}
+
+if (['rinkeby'].includes(argv.network) && INFURA_KEY === undefined) {
+  throw new Error(
+    `Could not find Infura key in env, unable to connect to network ${argv.network}`,
+  )
+}
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -22,10 +51,16 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true,
       blockGasLimit: 100000000,
       gas: 100000000
+    },
+    rinkeby: {
+      ...sharedNetworkConfig,
+      url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
     }
   },
   namedAccounts: {
-    deployer: 0,
+    deployer: {
+      default: 0
+    }
   }
 }
 
--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-
-export default {
-  clearMocks: true,
-  coverageDirectory: "coverage",
-  moduleDirectories: ["node_modules", "src"],
-  preset: "ts-jest",
-  verbose: true
-}

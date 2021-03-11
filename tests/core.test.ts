f340eea197e722e3fe70df54c8e3676c43553301
Germán Martínez <germaga92@gmail.com>
2021-03-11 13:06:53 +0100

    Add test cases for the read methods

---
+++ tests/core.test.ts
@@ -0,0 +1,81 @@
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import { BigNumber } from 'ethers'
+import { deployments, ethers, waffle } from 'hardhat'
+import EthersSafe from '../src/index'
+import { getSafeWithOwners } from './utils/setup'
+chai.use(chaiAsPromised)
+
+describe('Safe Core SDK', () => {
+  const [user1] = waffle.provider.getWallets()
+
+  const setupTests = deployments.createFixture(async ({ deployments }) => {
+    await deployments.fixture()
+    return {
+      safe: await getSafeWithOwners([user1.address, user1.address]),
+      chainId: (await waffle.provider.getNetwork()).chainId
+    }
+  })
+
+  describe('getContractVersion', async () => {
+    it('should return the Safe contract version', async () => {
+      const { safe } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, user1, safe.address)
+      const contractVersion = await safeSdk.getContractVersion()
+      chai.expect(contractVersion).to.be.eq('1.2.0')
+    })
+  })
+
+  describe('getAddress', async () => {
+    it('should return the Safe contract address', async () => {
+      const { safe } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, user1, safe.address)
+      chai.expect(safeSdk.getAddress()).to.be.eq(safe.address)
+    })
+  })
+
+  describe('getNetworkId', async () => {
+    it('should return the chainId of the current network', async () => {
+      const { safe, chainId } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, user1, safe.address)
+      chai.expect(await safeSdk.getNetworkId()).to.be.eq(chainId)
+    })
+  })
+
+  describe('getBalance', async () => {
+    it('should return the balance of the Safe contract', async () => {
+      const { safe } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, user1, safe.address)
+      chai.expect(await safeSdk.getBalance()).to.be.eq(0)
+      await user1.sendTransaction({
+        to: safe.address,
+        value: BigNumber.from(`${1e18}`).toHexString()
+      })
+      chai.expect(await safeSdk.getBalance()).to.be.eq(BigNumber.from(`${1e18}`))
+    })
+  })
+
+  describe('getModules', async () => {
+    it('should return an empty array if there are no modules enabled', async () => {
+      const { safe } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, user1, safe.address)
+      chai.expect((await safeSdk.getModules()).length).to.be.eq(0)
+    })
+
+    it.skip('should return all the enabled modules', async () => {
+      // TO-DO when enableModule() is added
+    })
+  })
+
+  describe('isModuleEnabled', async () => {
+    it('should return false if a module is not enabled', async () => {
+      const { safe } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, user1, safe.address)
+      chai.expect(await safeSdk.isModuleEnabled(user1.address)).to.be.false
+    })
+
+    it.skip('should return true if a module is enabled', async () => {
+      // TO-DO when enableModule() is added
+    })
+  })
+})

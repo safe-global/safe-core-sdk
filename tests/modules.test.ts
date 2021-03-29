72dbf2a629004141f1bd5b13773cefb6e8e54f8a
Germán Martínez <germaga92@gmail.com>
2021-03-29 17:10:26 +0200

    Add Safe modules tests

--- tests/modules.test.ts
+++ tests/modules.test.ts
@@ -1,8 +1,9 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import { deployments, ethers, waffle } from 'hardhat'
+import { ethers } from 'ethers'
+import { deployments, waffle } from 'hardhat'
 import EthersSafe from '../src'
-import { getSafeWithOwners } from './utils/setup'
+import { getDailyLimitModule, getSafeWithOwners, getSocialRecoveryModule } from './utils/setup'
 chai.use(chaiAsPromised)
 
 describe('Safe modules', () => {
@@ -11,39 +12,102 @@ describe('Safe modules', () => {
   const setupTests = deployments.createFixture(async ({ deployments }) => {
     await deployments.fixture()
     return {
-      safe: await getSafeWithOwners([user1.address, user2.address]),
+      safe: await getSafeWithOwners([user1.address]),
+      module1: await getDailyLimitModule(),
+      module2: await getSocialRecoveryModule()
     }
   })
 
   describe('getModules', async () => {
-    it('should return an empty array if there are no modules enabled', async () => {
-      const { safe } = await setupTests()
+    it('should return all the enabled modules', async () => {
+      const { safe, module1 } = await setupTests()
       const safeSdk = new EthersSafe(ethers, safe.address, user1)
       chai.expect((await safeSdk.getModules()).length).to.be.eq(0)
-    })
-
-    it.skip('should return all the enabled modules', async () => {
-      // TO-DO when enableModule() is added
+      const tx = await safeSdk.getEnableModuleTx(module1.address)
+      const txResponse = await safeSdk.executeTransaction(tx, { gasLimit: 10000000 })
+      await txResponse.wait()
+      chai.expect((await safeSdk.getModules()).length).to.be.eq(1)
     })
   })
 
   describe('isModuleEnabled', async () => {
-    it('should return false if a module is not enabled', async () => {
-      const { safe } = await setupTests()
+    it('should return true if a module is enabled', async () => {
+      const { safe, module1 } = await setupTests()
       const safeSdk = new EthersSafe(ethers, safe.address, user1)
-      chai.expect(await safeSdk.isModuleEnabled(user1.address)).to.be.false
+      chai.expect(await safeSdk.isModuleEnabled(module1.address)).to.be.false
+      const tx = await safeSdk.getEnableModuleTx(module1.address)
+      const txResponse = await safeSdk.executeTransaction(tx, { gasLimit: 10000000 })
+      await txResponse.wait()
+      chai.expect(await safeSdk.isModuleEnabled(module1.address)).to.be.true
     })
+  })
 
-    it.skip('should return true if a module is enabled', async () => {
-      // TO-DO when enableModule() is added
+  describe('getEnableModuleTx', async () => {
+    it('should fail when enabling a module that is already enabled', async () => {
+      const { safe, module1 } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, safe.address, user1)
+      const tx = await safeSdk.getEnableModuleTx(module1.address)
+      const txResponse = await safeSdk.executeTransaction(tx, { gasLimit: 10000000 })
+      await txResponse.wait()
+      await chai
+        .expect(safeSdk.getEnableModuleTx(module1.address))
+        .to.be.rejectedWith('Module provided is already enabled')
     })
   })
 
   describe('enableModule', async () => {
-    // TO-DO
+    it('should enable a Safe module', async () => {
+      const { safe, module1 } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, safe.address, user1)
+      chai.expect((await safeSdk.getModules()).length).to.be.eq(0)
+      chai.expect(await safeSdk.isModuleEnabled(module1.address)).to.be.false
+      const tx = await safeSdk.getEnableModuleTx(module1.address)
+      const txResponse = await safeSdk.executeTransaction(tx, { gasLimit: 10000000 })
+      await txResponse.wait()
+      chai.expect((await safeSdk.getModules()).length).to.be.eq(1)
+      chai.expect(await safeSdk.isModuleEnabled(module1.address)).to.be.true
+    })
+  })
+
+  describe('getDisableModuleTx', async () => {
+    it('should fail when disabling a module that is not enabled', async () => {
+      const { safe } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, safe.address, user1)
+      await chai
+        .expect(safeSdk.getDisableModuleTx('0x0000000000000000000000000000000000000001'))
+        .to.be.rejectedWith('Module provided is not enabled already')
+    })
   })
 
   describe('disableModule', async () => {
-    // TO-DO
+    it('should disable Safe modules', async () => {
+      const { module1, module2 } = await setupTests()
+      const safe = await getSafeWithOwners([user1.address])
+      const safeSdk = new EthersSafe(ethers, safe.address, user1)
+
+      const tx1 = await safeSdk.getEnableModuleTx(module1.address)
+      const txResponse1 = await safeSdk.executeTransaction(tx1, { gasLimit: 10000000 })
+      await txResponse1.wait()
+      const tx2 = await safeSdk.getEnableModuleTx(module2.address)
+      const txResponse2 = await safeSdk.executeTransaction(tx2, { gasLimit: 10000000 })
+      await txResponse2.wait()
+      chai.expect((await safeSdk.getModules()).length).to.be.eq(2)
+      chai.expect(await safeSdk.isModuleEnabled(module1.address)).to.be.true
+      chai.expect(await safeSdk.isModuleEnabled(module2.address)).to.be.true
+
+      const tx3 = await safeSdk.getDisableModuleTx(module1.address)
+      const txResponse3 = await safeSdk.executeTransaction(tx3, { gasLimit: 10000000 })
+      await txResponse3.wait()
+      chai.expect((await safeSdk.getModules()).length).to.be.eq(1)
+      chai.expect(await safeSdk.isModuleEnabled(module1.address)).to.be.false
+      chai.expect(await safeSdk.isModuleEnabled(module2.address)).to.be.true
+
+      const tx4 = await safeSdk.getDisableModuleTx(module2.address)
+      const txResponse4 = await safeSdk.executeTransaction(tx4, { gasLimit: 10000000 })
+      await txResponse4.wait()
+      chai.expect((await safeSdk.getModules()).length).to.be.eq(0)
+      chai.expect(await safeSdk.isModuleEnabled(module1.address)).to.be.false
+      chai.expect(await safeSdk.isModuleEnabled(module2.address)).to.be.false
+    })
   })
 })

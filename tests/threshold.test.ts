ac461fb7f41bd4f55722bd96b4cab4cb9c82d399
Germán Martínez <germaga92@gmail.com>
2021-04-05 14:26:14 +0200

    Add threshold tests

--- tests/threshold.test.ts
+++ tests/threshold.test.ts
@@ -11,7 +11,7 @@ describe('Safe Threshold', () => {
   const setupTests = deployments.createFixture(async ({ deployments }) => {
     await deployments.fixture()
     return {
-      safe: await getSafeWithOwners([user1.address, user2.address]),
+      safe: await getSafeWithOwners([user1.address])
     }
   })
 
@@ -19,11 +19,40 @@ describe('Safe Threshold', () => {
     it('should return the Safe threshold', async () => {
       const { safe } = await setupTests()
       const safeSdk = new EthersSafe(ethers, safe.address, user1)
-      chai.expect(await safeSdk.getThreshold()).to.be.eq(2)
+      chai.expect(await safeSdk.getThreshold()).to.be.eq(1)
     })
   })
 
-  describe('updateThreshold', async () => {
-    // TO-DO
+  describe('changeThreshold', async () => {
+    it('should fail if the threshold is bigger than the number of owners', async () => {
+      const { safe } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, safe.address, user1)
+      const newThreshold = 2
+      const numOwners = (await safeSdk.getOwners()).length
+      chai.expect(newThreshold).to.be.gt(numOwners)
+      await chai
+        .expect(safeSdk.getChangeThresholdTx(newThreshold))
+        .to.be.rejectedWith('Threshold cannot exceed owner count')
+    })
+
+    it('should fail if the threshold is not bigger than 0', async () => {
+      const { safe } = await setupTests()
+      const safeSdk = new EthersSafe(ethers, safe.address, user1)
+      const newThreshold = 0
+      await chai
+        .expect(safeSdk.getChangeThresholdTx(newThreshold))
+        .to.be.rejectedWith('Threshold needs to be greater than 0')
+    })
+
+    it('should change the threshold', async () => {
+      const safe = await getSafeWithOwners([user1.address, user2.address], 1)
+      const safeSdk = new EthersSafe(ethers, safe.address, user1)
+      chai.expect(await safeSdk.getThreshold()).to.be.eq(1)
+      const newThreshold = 2
+      const tx = await safeSdk.getChangeThresholdTx(newThreshold)
+      const txResponse = await safeSdk.executeTransaction(tx, { gasLimit: 10000000 })
+      await txResponse.wait()
+      chai.expect(await safeSdk.getThreshold()).to.be.eq(2)
+    })
   })
 })

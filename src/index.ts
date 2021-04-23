21b7c890772954055fe8d15e6cff30a22761a884
Germán Martínez <germaga92@gmail.com>
2021-04-23 12:11:59 +0200

    Remove unused types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import EthersSafe from './EthersSafe'
-import { ContractCallParams, SafeSettings } from './managers/types'
 import Safe from './Safe'
 import { SafeTransaction } from './utils/transactions'
 
 export default EthersSafe
-export { SafeTransaction, SafeSettings, Safe, ContractCallParams }
+export { SafeTransaction, Safe }

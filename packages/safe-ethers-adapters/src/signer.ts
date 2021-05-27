dbd203a1fdc54678c18cb5593099fa64b2e0a188
Richard Meissner <richard@gnosis.io>
2021-05-27 23:05:05 +0200

    Added example

--- packages/safe-ethers-adapters/src/signer.ts
+++ packages/safe-ethers-adapters/src/signer.ts
@@ -5,8 +5,11 @@ import { Provider, TransactionResponse, TransactionRequest, TransactionReceipt }
 import { Deferrable } from "@ethersproject/properties";
 import { getCreateCallDeployment } from "@gnosis.pm/safe-deployments";
 import EthersSafe, { Safe, SafeSignature, SafeTransaction, SafeTransactionDataPartial } from "@gnosis.pm/safe-core-sdk";
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 import { SafeTransactionData } from "@gnosis.pm/safe-core-sdk/dist/src/utils/transactions/SafeTransaction";
+import { poll } from "@ethersproject/web";
+
+const sleep = (duration: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, duration))
 
 export class SafeService {
 
@@ -18,10 +21,16 @@ export class SafeService {
         this.network = network ?? axios;
     }
 
-    async estimateSafeTx(safe: string, safeTx: SafeTransactionDataPartial): Promise<any> {
+    async estimateSafeTx(safe: string, safeTx: SafeTransactionDataPartial): Promise<BigNumber> {
         const url = `${this.serviceUrl}/api/v1/safes/${safe}/multisig-transactions/estimations/`
-        const resp = await this.network.post(url, safeTx)
-        return resp.data
+        try {
+            console.log({safeTx})
+            const resp = await this.network.post(url, safeTx)
+            return BigNumber.from(resp.data.safeTxGas)
+        } catch (e) {
+            console.log((e as AxiosError).response)
+            throw e
+        }
     }
 
     async getSafeTxDetails(safeTxHash: string): Promise<any> {
@@ -33,13 +42,19 @@ export class SafeService {
     async proposeTx(safeAddress: string, safeTxHash: string, safeTx: SafeTransaction, signature: SafeSignature): Promise<String> {
         const url = `${this.serviceUrl}/api/v1/safes/${safeAddress}/multisig-transactions/`
         const data = {
-            ...safeTx,
+            ...safeTx.data,
             contractTransactionHash: safeTxHash,
             sender: signature.signer,
             signature: signature.data
         }
-        const resp = await this.network.post(url, data)
-        return resp.data
+        try {
+            console.log({data})
+            const resp = await this.network.post(url, data)
+            return resp.data
+        } catch (e) {
+            console.log((e as AxiosError).response)
+            throw e
+        }
     }
 }
 
@@ -70,6 +85,7 @@ export class SafeEthersSigner extends VoidSigner {
 
     async buildTransactionResponse(safeTxHash: string, safeTx: SafeTransactionData): Promise<SafeTransactionResponse> {
         const connectedSafe = this.safe;
+        const connectedService = this.service;
         return {
             to: safeTx.to,
             value: BigNumber.from(safeTx.value), 
@@ -82,16 +98,34 @@ export class SafeEthersSigner extends VoidSigner {
             hash: safeTxHash,
             from: this.address,
             confirmations: 0,
-            wait: async (confirmations?: number): Promise<TransactionReceipt> => { 
-                return this._fail("SafeEthersSigner cannot wait yet", "wait") 
+            wait: async (confirmations?: number): Promise<TransactionReceipt> => {
+                while (true) {
+                    try {
+                        const txDetails = await connectedService.getSafeTxDetails(safeTxHash)
+                        if (txDetails.transactionHash) {
+                            this._checkProvider("sendTransaction")
+                            const receipt = await this.provider!!.waitForTransaction(txDetails.transactionHash, confirmations)
+                            // update status with Safe tx status and extract created contract
+                            // TODO load topic strings from abi
+                            const success = receipt.logs.find((log: any) => log.topics[0] === "0x442e715f626346e8c54381002da614f62bee8d27386535b2521ec8540898556e")
+                            receipt.status = !!success ? 1 : 0
+                            if (safeTx.to.toLowerCase() === this.createLibAddress.toLowerCase()) {
+                                const creationLog = receipt.logs.find((log: any) => log.topics[0] === "0x4db17dd5e4732fb6da34a148104a592783ca119a1e7bb8829eba6cbadef0b511")
+                                if(creationLog)
+                                    receipt.contractAddress = utils.getAddress("0x" + creationLog.data.slice(creationLog.data.length - 40))
+                            }
+                            return receipt
+                        }
+                    } catch (e) {
+                    }
+                    await sleep(5000)
+                }
             }
         }
     }
 
     // Populates all fields in a transaction, signs it and sends it to the network
     async sendTransaction(transaction: Deferrable<TransactionRequest>): Promise<SafeTransactionResponse> {
-        this._checkProvider("sendTransaction");
-
         const tx = await transaction
         let operation = 0
         let to = await tx.to
@@ -100,7 +134,7 @@ export class SafeEthersSigner extends VoidSigner {
         if (!to) {
             to = this.createLibAddress
             data = this.createLibInterface.encodeFunctionData("performCreate", [value, data])
-            if (!value.eq(0)) return this._fail("SafeEthersSigner cannot deploy contract with value", "sendTransaction");
+            value = BigNumber.from(0)
             operation = 1
         }
         const baseTx = {
@@ -112,7 +146,7 @@ export class SafeEthersSigner extends VoidSigner {
         const safeTxGas = await this.service.estimateSafeTx(this.address, baseTx)
         const safeTx = await this.safe.createTransaction({
             ...baseTx,
-            safeTxGas
+            safeTxGas: safeTxGas.toNumber()
         })
         const safeTxHash = await this.safe.getTransactionHash(safeTx)
         const signature = await this.safe.signTransactionHash(safeTxHash)

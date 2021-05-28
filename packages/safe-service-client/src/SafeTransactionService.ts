83c9ac9438241f415262ddb0330f549a2e367e14
Germán Martínez <germaga92@gmail.com>
2021-05-28 18:52:02 +0200

    Add Safe Transation Service interface

---
+++ packages/safe-service-client/src/SafeTransactionService.ts
@@ -0,0 +1,72 @@
+import { SafeSignature, SafeTransactionData } from '@gnosis.pm/safe-core-sdk-types'
+import {
+  MasterCopyResponse,
+  SafeBalanceResponse,
+  SafeBalanceUsdResponse,
+  SafeCollectibleResponse,
+  SafeCreationInfoResponse,
+  SafeDelegate,
+  SafeDelegateDelete,
+  SafeDelegateListResponse,
+  SafeInfoResponse,
+  SafeModuleTransactionListResponse,
+  SafeMultisigConfirmationListResponse,
+  SafeMultisigTransactionEstimate,
+  SafeMultisigTransactionEstimateResponse,
+  SafeMultisigTransactionListResponse,
+  SafeMultisigTransactionResponse,
+  SafeServiceInfoResponse,
+  TokenInfoListResponse,
+  TokenInfoResponse,
+  TransferListResponse
+} from './types/safeTransactionServiceTypes'
+
+interface SafeTransactionService {
+  // About
+  getServiceInfo(): Promise<SafeServiceInfoResponse>
+  getServiceMasterCopiesInfo(): Promise<MasterCopyResponse>
+
+  // Data decoder
+  decodeData(data: string): Promise<any>
+
+  // Owners
+  getSafesByOwner(ownerAddress: string): Promise<string[]>
+
+  // Multisig transactions
+  getTransaction(safeTxHash: string): Promise<SafeMultisigTransactionResponse>
+  getTransactionConfirmations(safeTxHash: string): Promise<SafeMultisigConfirmationListResponse>
+  confirmTransaction(safeTxHash: string, signature: string): Promise<any>
+
+  // Safes
+  getSafeInfo(safeAddress: string): Promise<SafeInfoResponse>
+  getSafeDelegates(safeAddress: string): Promise<SafeDelegateListResponse>
+  addSafeDelegate(safeAddress: string, delegate: SafeDelegate): Promise<any>
+  removeSafeDelegate(safeAddress: string, delegate: SafeDelegateDelete): Promise<any>
+  getSafeCreationInfo(safeAddress: string): Promise<SafeCreationInfoResponse>
+  estimateSafeTransaction(
+    safeAddress: string,
+    safeTransaction: SafeMultisigTransactionEstimate
+  ): Promise<SafeMultisigTransactionEstimateResponse>
+  proposeTransaction(
+    safeAddress: string,
+    transaction: SafeTransactionData,
+    transactionHash: string,
+    signature: SafeSignature
+  ): Promise<string>
+  getIncomingTransactions(safeAddress: string): Promise<TransferListResponse>
+  getModuleTransactions(safeAddress: string): Promise<SafeModuleTransactionListResponse>
+  getMultisigTransactions(safeAddress: string): Promise<SafeMultisigTransactionListResponse>
+  getPendingTransactions(
+    safeAddress: string,
+    currentNonce?: number
+  ): Promise<SafeMultisigTransactionListResponse>
+  getBalances(safeAddress: string): Promise<SafeBalanceResponse[]>
+  getUsdBalances(safeAddress: string): Promise<SafeBalanceUsdResponse[]>
+  getCollectibles(safeAddress: string): Promise<SafeCollectibleResponse[]>
+
+  // Tokens
+  getTokens(): Promise<TokenInfoListResponse>
+  getToken(tokenAddress: string): Promise<TokenInfoResponse>
+}
+
+export default SafeTransactionService

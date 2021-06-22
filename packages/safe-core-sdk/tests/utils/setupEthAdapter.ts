93ed0d4fe8523c9475d3f9ae58f1eb90a2f6f9b4
Germán Martínez <germaga92@gmail.com>
2021-06-22 19:05:19 +0200

    Fix test connection agains ganache/hardhat

--- packages/safe-core-sdk/tests/utils/setupEthAdapter.ts
+++ packages/safe-core-sdk/tests/utils/setupEthAdapter.ts
@@ -1,24 +1,16 @@
-import { Provider } from '@ethersproject/providers'
 import { Signer } from 'ethers'
-import { ethers } from 'hardhat'
-import Web3 from 'web3'
+import { ethers, web3 } from 'hardhat'
 import { EthAdapter, EthersAdapter, Web3Adapter } from '../../src'
 
-export async function getEthAdapter(providerOrSigner?: Provider | Signer): Promise<EthAdapter> {
-  console.log(`USING ${process.env.ETH_LIB} AND ${process.env.TEST_NETWORK}`)
+export async function getEthAdapter(signer: Signer): Promise<EthAdapter> {
   let ethAdapter: EthAdapter
   switch (process.env.ETH_LIB) {
     case 'web3':
-      const web3 = new Web3('http://localhost:8545')
-      let signerAddress: string | undefined = undefined
-      if (providerOrSigner instanceof Signer) {
-        signerAddress = await (providerOrSigner as Signer).getAddress()
-      }
-      console.log({ signerAddress })
+      const signerAddress = await signer.getAddress()
       ethAdapter = new Web3Adapter({ web3, signerAddress })
       break
     case 'ethers':
-      ethAdapter = new EthersAdapter({ ethers, providerOrSigner })
+      ethAdapter = new EthersAdapter({ ethers, signer })
       break
     default:
       throw new Error('Ethereum library not supported')

3815227a4131e6e0eb11514d6b4fbf9d57203055
Germán Martínez <germaga92@gmail.com>
2021-07-01 10:48:06 +0200

    Update Eth Adapters to handle Gnosis Safe Proxy Factory contract

--- packages/safe-core-sdk/src/ethereumLibs/EthAdapter.ts
+++ packages/safe-core-sdk/src/ethereumLibs/EthAdapter.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from 'ethers'
-import { ContractNetworkConfig } from '../configuration/contracts'
 import GnosisSafeContract from '../contracts/GnosisSafe/GnosisSafeContract'
+import GnosisSafeProxyFactoryContract from '../contracts/GnosisSafeProxyFactory/GnosisSafeProxyFactoryContract'
 import MultiSendContract from '../contracts/MultiSend/MultiSendContract'
 import { Abi } from '../utils/types'
 
@@ -22,11 +22,10 @@ interface EthAdapter {
   isAddress(address: string): boolean
   getBalance(address: string): Promise<BigNumber>
   getChainId(): Promise<number>
-  getSafeContracts(
-    safeAddress: string,
-    contracts: ContractNetworkConfig
-  ): Promise<GnosisSafeContracts>
   getContract(address: string, abi: Abi): any
+  getSafeContract(safeAddress: string): Promise<GnosisSafeContract>
+  getMultiSendContract(multiSendAddress: string): Promise<MultiSendContract>
+  getGnosisSafeProxyFactoryContract(proxyFactoryAddress: string): Promise<GnosisSafeProxyFactoryContract>
   getContractCode(address: string): Promise<string>
   getTransaction(transactionHash: string): Promise<any>
   getSignerAddress(): Promise<string>

1a97e32f6f35c3ce6a4d8cad6380174836f515bf
Germán Martínez <germaga92@gmail.com>
2021-07-30 15:24:16 +0200

    Handle errors

--- packages/safe-service-client/src/utils/httpRequests.ts
+++ packages/safe-service-client/src/utils/httpRequests.ts
@@ -17,8 +17,24 @@ export async function sendRequest<T>({ url, method, body }: HttpRequest): Promis
   try {
     response = await axios[method](url, body)
   } catch (error) {
-    console.log(`${error.response.status} ${error.response.statusText}`)
-    return error.response.data
+    if (error.response) {
+      console.log(error.response)
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      if (error.response.data) {
+        throw new Error(error.response.data.message)
+      } else {
+        throw new Error(error.response.statusText)
+      }
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      throw new Error('Connection error')
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      throw new Error(error.message)
+    }
   }
   return response.data as T
 }

5950b7ab48154db03b5d59c0893439b45816c0e3
Germán Martínez <germaga92@gmail.com>
2021-08-30 16:47:18 +0200

    Update Safe Ethers adapters examples

--- packages/safe-ethers-adapters/examples/interaction.ts
+++ packages/safe-ethers-adapters/examples/interaction.ts
@@ -1,27 +1,28 @@
-import { SafeEthersSigner, SafeService } from '../src'
-import dotenv from 'dotenv'
-import { Wallet } from '@ethersproject/wallet'
-import { JsonRpcProvider } from '@ethersproject/providers'
 import { Contract } from '@ethersproject/contracts'
-import EthersSafe from '@gnosis.pm/safe-core-sdk'
+import { JsonRpcProvider } from '@ethersproject/providers'
+import { Wallet } from '@ethersproject/wallet'
+import Safe, { EthersAdapter } from '@gnosis.pm/safe-core-sdk'
+import dotenv from 'dotenv'
 import { ethers } from 'ethers'
+import { SafeEthersSigner, SafeService } from '../src'
 
 dotenv.config()
 
 const sample = async () => {
     console.log("Setup provider")
     const provider = new JsonRpcProvider(process.env.JSON_RPC!!)
-    console.log("Setup SafeServices")
+    console.log("Setup SafeService")
     const service = new SafeService(process.env.SERVICE_URL!!)
     console.log("Setup Signer")
     const signer = new Wallet(process.env.SIGNER_KEY!!, provider)
     console.log("Setup SafeEthersSigner")
-    const safe = await EthersSafe.create({ ethers, safeAddress: process.env.DEPLOYER_SAFE!!, providerOrSigner: signer })
+    const ethAdapter = new EthersAdapter({ ethers, signer })
+    const safe = await Safe.create({ ethAdapter, safeAddress: process.env.DEPLOYER_SAFE!! })
     const safeSigner = new SafeEthersSigner(safe, service, provider)
     const contract = new Contract("0xe50c6391a6cb10f9B9Ef599aa1C68C82dD88Bd91", ["function pin(string newMessage)"], safeSigner)
     const proposedTx = await contract.functions.pin(`Local time: ${new Date().toLocaleString()}`)
     console.log("USER ACTION REQUIRED")
-    console.log("Go to the Gnosis Safe Web App to confirm the transcation")
+    console.log("Go to the Gnosis Safe Web App to confirm the transaction")
     console.log(await proposedTx.wait())
     console.log("Transaction has been executed")
 }

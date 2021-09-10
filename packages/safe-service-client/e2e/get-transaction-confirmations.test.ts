696d9f0ef1265c0922233bdf3fd72f13d06638bb
Germán Martínez <germaga92@gmail.com>
2021-09-10 09:31:01 +0200

    Add transactions tests

---
+++ packages/safe-service-client/e2e/get-transaction-confirmations.test.ts
@@ -0,0 +1,33 @@
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import SafeServiceClient, { SafeMultisigConfirmationListResponse } from '../src'
+import config from './config'
+
+chai.use(chaiAsPromised)
+
+describe('getTransactionConfirmations', () => {
+  const serviceSdk = new SafeServiceClient(config.baseUrl)
+
+  it('should fail if safeTxHash is empty', async () => {
+    const safeTxHash = ''
+    await chai
+      .expect(serviceSdk.getTransactionConfirmations(safeTxHash))
+      .to.be.rejectedWith('Invalid safeTxHash')
+  })
+
+  it('should return an empty array if the safeTxHash is not found', async () => {
+    const safeTxHash = '0x'
+    const transactionConfirmations: SafeMultisigConfirmationListResponse =
+      await serviceSdk.getTransactionConfirmations(safeTxHash)
+    chai.expect(transactionConfirmations.count).to.be.equal(0)
+    chai.expect(transactionConfirmations.results.length).to.be.equal(0)
+  })
+
+  it('should return the transaction with the given safeTxHash', async () => {
+    const safeTxHash = '0xb22be4e57718560c89de96acd1acefe55c2673b31a7019a374ebb1d8a2842f5d'
+    const transactionConfirmations: SafeMultisigConfirmationListResponse =
+      await serviceSdk.getTransactionConfirmations(safeTxHash)
+    chai.expect(transactionConfirmations.count).to.be.equal(2)
+    chai.expect(transactionConfirmations.results.length).to.be.equal(2)
+  })
+})

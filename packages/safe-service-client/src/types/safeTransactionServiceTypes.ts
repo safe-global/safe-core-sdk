4ba68376ae75eb8846422e75692d1f243d24a8d5
Germán Martínez <germaga92@gmail.com>
2021-09-29 13:10:26 +0200

    Make signatures prop in SafeMultisigTransactionResponse nullable

--- packages/safe-service-client/src/types/safeTransactionServiceTypes.ts
+++ packages/safe-service-client/src/types/safeTransactionServiceTypes.ts
@@ -134,7 +134,7 @@ export type SafeMultisigTransactionResponse = {
   readonly dataDecoded?: string
   readonly confirmationsRequired: number
   readonly confirmations?: SafeMultisigConfirmationResponse[]
-  readonly signatures: string
+  readonly signatures?: string
 }
 
 export type SafeMultisigTransactionListResponse = {

7f3ea47e2be54ef0a88bddfca58105db3cb22c7e
Germán Martínez <germaga92@gmail.com>
2022-02-01 07:12:07 +0100

    Add transaction options to safe-ethers-lib

--- packages/safe-ethers-lib/src/contracts/GnosisSafeProxyFactory/GnosisSafeProxyFactoryEthersContract.ts
+++ packages/safe-ethers-lib/src/contracts/GnosisSafeProxyFactory/GnosisSafeProxyFactoryEthersContract.ts
@@ -1,7 +1,15 @@
 import { Event } from '@ethersproject/contracts'
-import { CreateProxyProps, GnosisSafeProxyFactoryContract } from '@gnosis.pm/safe-core-sdk-types'
+import { GnosisSafeProxyFactoryContract } from '@gnosis.pm/safe-core-sdk-types'
 import { ProxyFactory as ProxyFactory_V1_1_1 } from '../../../typechain/src/ethers-v5/v1.1.1/ProxyFactory'
 import { ProxyFactory as ProxyFactory_V1_3_0 } from '../../../typechain/src/ethers-v5/v1.3.0/ProxyFactory'
+import { EthersTransactionOptions } from '../../types'
+
+export interface CreateProxyProps {
+  safeMasterCopyAddress: string
+  initializer: string
+  saltNonce: number
+  options?: EthersTransactionOptions
+}
 
 class GnosisSafeProxyFactoryEthersContract implements GnosisSafeProxyFactoryContract {
   constructor(public contract: ProxyFactory_V1_3_0 | ProxyFactory_V1_1_1) {}
@@ -18,6 +26,15 @@ class GnosisSafeProxyFactoryEthersContract implements GnosisSafeProxyFactoryCont
   }: CreateProxyProps): Promise<string> {
     if (saltNonce < 0) {
       throw new Error('saltNonce must be greater than 0')
+    }
+    if (options && !options.gasLimit) {
+      options.gasLimit = await this.estimateGas(
+        'createProxyWithNonce',
+        [safeMasterCopyAddress, initializer, saltNonce],
+        {
+          ...options
+        }
+      )
     }
     const txResponse = await this.contract.createProxyWithNonce(
       safeMasterCopyAddress,
@@ -38,6 +55,14 @@ class GnosisSafeProxyFactoryEthersContract implements GnosisSafeProxyFactoryCont
 
   encode(methodName: string, params: any[]): string {
     return (this.contract as any).interface.encodeFunctionData(methodName, params)
+  }
+
+  async estimateGas(
+    methodName: string,
+    params: any[],
+    options: EthersTransactionOptions
+  ): Promise<number> {
+    return (await (this.contract.estimateGas as any)[methodName](...params, options)).toNumber()
   }
 }
 
--- packages/safe-ethers-lib/src/index.ts
+++ packages/safe-ethers-lib/src/index.ts
@@ -1,4 +1,7 @@
+import { EthersTransactionResult } from './contracts/GnosisSafe/GnosisSafeContractEthers'
+import { CreateProxyProps } from './contracts/GnosisSafeProxyFactory/GnosisSafeProxyFactoryEthersContract'
 import EthersAdapter, { EthersAdapterConfig } from './EthersAdapter'
+import { EthersTransactionOptions } from './types'
 
 export default EthersAdapter
-export { EthersAdapterConfig }
+export { EthersAdapterConfig, EthersTransactionOptions, EthersTransactionResult, CreateProxyProps }

724eb5cf1857c444fe4d21e7ee7a31087324ca8b
Germán Martínez <germaga92@gmail.com>
2022-03-08 18:48:33 +0100

    Add script to sync EIP-3770 config with safe-deployments

---
+++ packages/safe-core-sdk-utils/scripts/checkSafeDeployments.ts
@@ -0,0 +1,29 @@
+import { DeploymentFilter, getSafeSingletonDeployment } from '@gnosis.pm/safe-deployments'
+import { networks } from '../src/eip-3770/config'
+
+const safeVersion = '1.3.0'
+
+function getSafeDeploymentNetworks(): string[] {
+  const filters: DeploymentFilter = { version: safeVersion, released: true }
+  const singletons = getSafeSingletonDeployment(filters)
+  if (!singletons) {
+    throw new Error('Empty Safe Deployments')
+  }
+  return Object.keys(singletons.networkAddresses)
+}
+
+function getLocalNetworksConfig(): string[] {
+  return networks.map(network => network.chainId.toString())
+}
+
+function checkConfigDiff() {
+  const safeDeployments = getSafeDeploymentNetworks()
+  const localNetworks = getLocalNetworksConfig()
+  if (safeDeployments.length !== localNetworks.length) {
+    const chainIdsDiff = safeDeployments.filter(chainId => !localNetworks.includes(chainId))
+    const errorMessage = `EIP-3770 local config is missing chainIds: ${chainIdsDiff}`
+    throw new Error(errorMessage)
+  }
+}
+
+checkConfigDiff()

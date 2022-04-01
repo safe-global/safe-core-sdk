b22dfe20d4fb2e219568a23c1a7a7e9d6c3fc437
Germán Martínez <germaga92@gmail.com>
2022-04-01 12:10:56 +0100

    Add Safe deployment callback to safe-web3-lib

--- packages/safe-web3-lib/src/types.ts
+++ packages/safe-web3-lib/src/types.ts
@@ -1,5 +1,13 @@
+import { BaseTransactionResult } from '@gnosis.pm/safe-core-sdk-types'
+import { PromiEvent, TransactionReceipt } from 'web3-core/types'
+
 export interface Web3TransactionOptions {
   from?: string
   gas?: number | string
   gasPrice?: number | string
+}
+
+export interface Web3TransactionResult extends BaseTransactionResult {
+  promiEvent: PromiEvent<TransactionReceipt>
+  options?: Web3TransactionOptions
 }

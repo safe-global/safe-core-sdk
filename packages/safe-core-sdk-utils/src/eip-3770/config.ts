435a0ae30d96fdee7a97069fb4600fbf58de5b7c
Germán Martínez <germaga92@gmail.com>
2022-06-07 12:19:17 +0200

    Update EIP3770 networks config

--- packages/safe-core-sdk-utils/src/eip-3770/config.ts
+++ packages/safe-core-sdk-utils/src/eip-3770/config.ts
@@ -16,6 +16,8 @@ export const networks: NetworkShortName[] = [
   { chainId: 42, shortName: 'kov' },
   { chainId: 56, shortName: 'bnb' },
   { chainId: 69, shortName: 'okov' },
+  { chainId: 82, shortName: 'meter' },
+  { chainId: 83, shortName: 'meter-test' },
   { chainId: 100, shortName: 'gno' },
   { chainId: 106, shortName: 'vlx' },
   { chainId: 111, shortName: 'etl' },
@@ -24,14 +26,18 @@ export const networks: NetworkShortName[] = [
   { chainId: 137, shortName: 'matic' },
   { chainId: 246, shortName: 'ewt' },
   { chainId: 288, shortName: 'boba' },
+  { chainId: 300, shortName: 'ogn' },
   { chainId: 588, shortName: 'metis-stardust' },
+  { chainId: 1008, shortName: 'eun' },
   { chainId: 1088, shortName: 'metis-andromeda' },
   { chainId: 1284, shortName: 'mbeam' },
   { chainId: 1285, shortName: 'mriver' },
   { chainId: 1287, shortName: 'mbase' },
+  { chainId: 1984, shortName: 'euntest' },
   { chainId: 4002, shortName: 'tftm' },
   { chainId: 7341, shortName: 'shyft' },
-  { chainId: 9000, shortName: 'evmos' },
+  { chainId: 9000, shortName: 'evmos-testnet' },
+  { chainId: 9001, shortName: 'evmos' },
   { chainId: 11437, shortName: 'shyftt' },
   { chainId: 12357, shortName: 'rei-testnet' },
   { chainId: 42161, shortName: 'arb1' },
@@ -45,7 +51,9 @@ export const networks: NetworkShortName[] = [
   { chainId: 1313161554, shortName: 'aurora' },
   { chainId: 1313161555, shortName: 'aurora-testnet' },
   { chainId: 1666600000, shortName: 'hmy-s0' },
-  { chainId: 1666700000, shortName: 'hmy-b-s0' }
+  { chainId: 1666700000, shortName: 'hmy-b-s0' },
+  { chainId: 11297108099, shortName: 'tpalm' },
+  { chainId: 11297108109, shortName: 'palm' }
 ]
 
 if (process.env.TEST_NETWORK === 'hardhat') {

56095ac286fd0afd2967cf1862ff99eb08a01a69
Germán Martínez <germaga92@gmail.com>
2022-07-27 12:19:58 +0200

    Validate SafeDeploymentConfig

--- packages/safe-core-sdk/src/safeFactory/utils.ts
+++ packages/safe-core-sdk/src/safeFactory/utils.ts
@@ -1,8 +1,12 @@
-import { SafeAccountConfig } from './'
+import { SafeAccountConfig, SafeDeploymentConfig } from './'
 
 export const validateSafeAccountConfig = ({ owners, threshold }: SafeAccountConfig): void => {
   if (owners.length <= 0) throw new Error('Owner list must have at least one owner')
   if (threshold <= 0) throw new Error('Threshold must be greater than or equal to 1')
   if (threshold > owners.length)
     throw new Error('Threshold must be lower than or equal to owners length')
+}
+
+export const validateSafeDeploymentConfig = ({ saltNonce }: SafeDeploymentConfig): void => {
+  if (saltNonce < 0) throw new Error('saltNonce must be greater than 0')
 }

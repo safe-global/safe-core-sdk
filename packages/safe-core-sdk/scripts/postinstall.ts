e5314909cbbe0e756452d6fae60925a0d47b340b
Germán Martínez <germaga92@gmail.com>
2022-08-02 13:04:31 +0200

    Update signTransaction warning message

--- packages/safe-core-sdk/scripts/postinstall.ts
+++ packages/safe-core-sdk/scripts/postinstall.ts
@@ -1,6 +1,6 @@
 function postInstall() {
   console.warn(
-    'WARNING! "signTransaction" method now *returns* the signed Safe transaction. Update your code according to the new documentation: https://github.com/safe-global/safe-core-sdk/tree/main/packages/safe-core-sdk#signtransaction'
+    'WARNING! "signTransaction" method now *returns* the signed Safe transaction. Update your code according to the new documentation: https://github.com/safe-global/safe-core-sdk/tree/main/packages/safe-core-sdk#signtransaction. In >=v3.0.0 the signature will only be added to the returned object, not the one that is passed as params.'
   )
 }
 

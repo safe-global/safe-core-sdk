68a25a85ae72e554e81d74d22959eec4350c5669
Germán Martínez <germaga92@gmail.com>
2022-08-02 13:04:31 +0200

    Update signTransaction warning message

--- packages/safe-core-sdk/src/Safe.ts
+++ packages/safe-core-sdk/src/Safe.ts
@@ -397,7 +397,7 @@ class Safe {
 
     // TO-DO: Remove in v3.0.0 {
     console.warn(
-      'WARNING! "signTransaction" method now *returns* the signed Safe transaction. Update your code according to the new documentation: https://github.com/safe-global/safe-core-sdk/tree/main/packages/safe-core-sdk#signtransaction'
+      'WARNING! "signTransaction" method now *returns* the signed Safe transaction. Update your code according to the new documentation: https://github.com/safe-global/safe-core-sdk/tree/main/packages/safe-core-sdk#signtransaction. In >=v3.0.0 the signature will only be added to the returned object, not the one that is passed as params.'
     )
     safeTransaction.addSignature(signature)
     // }

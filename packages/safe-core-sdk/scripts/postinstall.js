968e88e7ff56eed83ab43b18d3870082db1860d9
Germán Martínez <germaga92@gmail.com>
2022-08-03 12:31:01 +0200

    Remove postinstall script

--- packages/safe-core-sdk/scripts/postinstall.js
+++
@@ -1,7 +0,0 @@
-function postInstall() {
-  console.warn(
-    `⚠️ the "signTransaction" method now returns a signed Safe transaction. Please update your code according to the new documentation: https://github.com/safe-global/safe-core-sdk/tree/main/packages/safe-core-sdk#signtransaction. In >=v3.0.0, the signature will be added only to the return transaction object, and not to the one passed as an argument.`
-  )
-}
-
-postInstall()

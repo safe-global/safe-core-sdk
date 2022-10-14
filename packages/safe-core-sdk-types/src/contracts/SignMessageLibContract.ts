a299578ec9a5f8623cca47475ca5e76891e04a6d
Germán Martínez <germaga92@gmail.com>
2022-10-14 17:01:58 +0200

    Add SignTMessageLibContract interface

---
+++ packages/safe-core-sdk-types/src/contracts/SignMessageLibContract.ts
@@ -0,0 +1,9 @@
+import { TransactionOptions, TransactionResult } from '../types'
+
+export interface SignMessageLibContract {
+  getAddress(): string
+  signMessage(data: string, options?: TransactionOptions): Promise<TransactionResult>
+  getMessageHash(message: string): Promise<string>
+  encode(methodName: any, params: any): string
+  estimateGas(methodName: string, params: any[], options: TransactionOptions): Promise<number>
+}

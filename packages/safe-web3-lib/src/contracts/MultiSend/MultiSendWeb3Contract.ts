6792e9a48de1f0c3c5400e5061345846a7d41b40
Germán Martínez <germaga92@gmail.com>
2022-11-08 12:12:39 +0100

    Add MultiSend v1.0.0 web3 and ethers class

--- packages/safe-web3-lib/src/contracts/MultiSend/MultiSendWeb3Contract.ts
+++ packages/safe-web3-lib/src/contracts/MultiSend/MultiSendWeb3Contract.ts
@@ -1,9 +1,10 @@
 import { MultiSendContract } from '@gnosis.pm/safe-core-sdk-types'
+import { Multi_send as MultiSend_V1_0_0 } from '../../../typechain/src/web3-v1/v1.0.0/Multi_send'
 import { Multi_send as MultiSend_V1_1_1 } from '../../../typechain/src/web3-v1/v1.1.1/Multi_send'
 import { Multi_send as MultiSend_V1_3_0 } from '../../../typechain/src/web3-v1/v1.3.0/Multi_send'
 
 abstract class MultiSendWeb3Contract implements MultiSendContract {
-  constructor(public contract: MultiSend_V1_3_0 | MultiSend_V1_1_1) {}
+  constructor(public contract: MultiSend_V1_3_0 | MultiSend_V1_1_1 | MultiSend_V1_0_0) {}
 
   getAddress(): string {
     return this.contract.options.address

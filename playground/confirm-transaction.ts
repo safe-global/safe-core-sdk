1580017c25e24f464551bb515ea96cb158f0ccec
Germán Martínez <6764315+germartinez@users.noreply.github.com>
2022-11-30 11:44:45 +0000

    Add confirm-transaction playground script

---
+++ playground/confirm-transaction.ts
@@ -0,0 +1,66 @@
+import Safe from '@safe-global/safe-core-sdk'
+import EthersAdapter from '@safe-global/safe-ethers-lib'
+import SafeServiceClient from '@safe-global/safe-service-client'
+import { ethers } from 'ethers'
+
+// This file can be used to play around with the Safe Core SDK
+
+interface Config {
+  RPC_URL: string
+  SIGNER_ADDRESS_PRIVATE_KEY: string
+  SAFE_ADDRESS: string
+  TX_SERVICE_URL: string
+  SAFE_TX_HASH: string
+}
+
+const config: Config = {
+  RPC_URL: 'https://goerli.infura.io/v3/<INFURA_KEY>',
+  SIGNER_ADDRESS_PRIVATE_KEY: '<SIGNER_ADDRESS_PRIVATE_KEY>',
+  SAFE_ADDRESS: '<SAFE_ADDRESS>',
+  TX_SERVICE_URL: 'https://safe-transaction-goerli.safe.global/', // Check https://docs.gnosis-safe.io/backend/available-services
+  SAFE_TX_HASH: '<SAFE_TX_HASH>'
+}
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(config.RPC_URL)
+  const signer = new ethers.Wallet(config.SIGNER_ADDRESS_PRIVATE_KEY, provider)
+
+  // Create EthAdapter instance
+  const ethAdapter = new EthersAdapter({
+    ethers,
+    signerOrProvider: signer
+  })
+
+  // Create Safe instance
+  const safe = await Safe.create({
+    ethAdapter,
+    safeAddress: config.SAFE_ADDRESS
+  })
+
+  // Create Safe Service Client instance
+  const service = new SafeServiceClient({
+    txServiceUrl: config.TX_SERVICE_URL,
+    ethAdapter
+  })
+
+  // Get the transaction
+  const transaction = await service.getTransaction(config.SAFE_TX_HASH)
+  // const transactions = await service.getPendingTransactions()
+  // const transactions = await service.getIncomingTransactions()
+  // const transactions = await service.getMultisigTransactions()
+  // const transactions = await service.getModuleTransactions()
+  // const transactions = await service.getAllTransactions()
+
+  const safeTxHash = transaction.transactionHash
+  const signature = await safe.signTransactionHash(safeTxHash)
+
+  // Confirm the Safe transaction
+  const signatureResponse = await service.confirmTransaction(safeTxHash, signature.data)
+
+  const signerAddress = await signer.getAddress()
+  console.log('Added a new signature to transaction with safeTxGas:', config.SAFE_TX_HASH)
+  console.log('- Signer:', signerAddress)
+  console.log('- Signer signature:', signatureResponse.signature)
+}
+
+main()

531ea8ab40ab0618bd2045090d0eb44c6be21d89
Germán Martínez <germaga92@gmail.com>
2023-01-13 18:49:24 +0100

    Set eth_sign as a feature for Safes >= v1.1.0

--- packages/safe-core-sdk/src/utils/safeVersions.ts
+++ packages/safe-core-sdk/src/utils/safeVersions.ts
@@ -2,12 +2,14 @@ import semverSatisfies from 'semver/functions/satisfies'
 
 export enum FEATURES {
   SAFE_TX_GAS_OPTIONAL,
-  SAFE_TX_GUARDS
+  SAFE_TX_GUARDS,
+  ETH_SIGN
 }
 
 const FEATURES_BY_VERSION: Record<string, string> = {
   [FEATURES.SAFE_TX_GAS_OPTIONAL]: '>=1.3.0',
-  [FEATURES.SAFE_TX_GUARDS]: '>=1.3.0'
+  [FEATURES.SAFE_TX_GUARDS]: '>=1.3.0',
+  [FEATURES.ETH_SIGN]: '>=1.1.0'
 }
 
 const isEnabledByVersion = (feature: FEATURES, version: string): boolean => {

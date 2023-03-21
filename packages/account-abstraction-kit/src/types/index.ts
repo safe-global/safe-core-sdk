7f0f22bc274db70edf8e8c425c3ce550aa306a99
Germán Martínez <germaga92@gmail.com>
2023-03-21 20:53:19 +0100

    Export contract types from ethAdapters

--- packages/account-abstraction-kit/src/types/index.ts
+++ packages/account-abstraction-kit/src/types/index.ts
@@ -8,26 +8,3 @@ export enum OperationType {
 export interface AccountAbstractionConfig {
   relayAdapter: RelayAdapter
 }
-
-export interface MetaTransactionData {
-  to: string
-  value: string
-  data: string
-  operation?: OperationType
-}
-
-export interface SafeTransactionData extends MetaTransactionData {
-  operation: OperationType
-  safeTxGas: number
-  baseGas: number
-  gasPrice: number
-  gasToken: string
-  refundReceiver: string
-  nonce: number
-}
-
-export interface SafeSetupConfig {
-  owners: string[]
-  threshold: number
-  fallbackHandlerAddress: string
-}

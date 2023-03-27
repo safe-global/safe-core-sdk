b25296b3809d0bcc5868f5ffe1da09d8b047dbc3
Germán Martínez <germaga92@gmail.com>
2023-03-27 12:28:30 +0200

    Refactor fallbackHandlerManager

--- packages/protocol-kit/src/managers/fallbackHandlerManager.ts
+++ packages/protocol-kit/src/managers/fallbackHandlerManager.ts
@@ -1,14 +1,14 @@
 import { EthAdapter, GnosisSafeContract } from '@safe-global/safe-core-sdk-types'
-import { SAFE_FEATURES, hasSafeFeature, isZeroAddress, sameString } from '../utils'
+import { hasSafeFeature, isZeroAddress, SAFE_FEATURES, sameString } from '../utils'
 import { ZERO_ADDRESS } from '../utils/constants'
 
 class FallbackHandlerManager {
   #ethAdapter: EthAdapter
-  #safeContract: GnosisSafeContract
+  #safeContract?: GnosisSafeContract
   // keccak256("fallback_manager.handler.address")
   #slot = '0x6c9a6c4a39284e37ed1cf53d337577d14212a4870fb976a4366c693b939918d5'
 
-  constructor(ethAdapter: EthAdapter, safeContract: GnosisSafeContract) {
+  constructor(ethAdapter: EthAdapter, safeContract?: GnosisSafeContract) {
     this.#ethAdapter = ethAdapter
     this.#safeContract = safeContract
   }
@@ -36,6 +36,9 @@ class FallbackHandlerManager {
   }
 
   async getFallbackHandler(): Promise<string> {
+    if (!this.#safeContract) {
+      throw new Error('Safe is not deployed')
+    }
     const safeVersion = await this.#safeContract.getVersion()
     if (hasSafeFeature(SAFE_FEATURES.SAFE_FALLBACK_HANDLER, safeVersion)) {
       return this.#ethAdapter.getStorageAt(this.#safeContract.getAddress(), this.#slot)
@@ -47,6 +50,9 @@ class FallbackHandlerManager {
   }
 
   async encodeEnableFallbackHandlerData(fallbackHandlerAddress: string): Promise<string> {
+    if (!this.#safeContract) {
+      throw new Error('Safe is not deployed')
+    }
     this.validateFallbackHandlerAddress(fallbackHandlerAddress)
     const currentFallbackHandler = await this.getFallbackHandler()
     this.validateFallbackHandlerIsNotEnabled(currentFallbackHandler, fallbackHandlerAddress)
@@ -54,6 +60,9 @@ class FallbackHandlerManager {
   }
 
   async encodeDisableFallbackHandlerData(): Promise<string> {
+    if (!this.#safeContract) {
+      throw new Error('Safe is not deployed')
+    }
     const currentFallbackHandler = await this.getFallbackHandler()
     this.validateFallbackHandlerIsEnabled(currentFallbackHandler)
     return this.#safeContract.encode('setFallbackHandler', [ZERO_ADDRESS])

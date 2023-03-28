667a552cd63fa2f7fccbba06aa1d0fe818c50d98
Yago Pérez Vázquez <yago@safe.global>
2023-03-28 17:02:26 +0200

    Remove auto generated files

--- packages/api-kit/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import SafeApiKit, { SafeApiKitConfig } from './SafeApiKit';
-export * from './types/safeTransactionServiceTypes';
-export { SafeApiKitConfig };
-export default SafeApiKit;
--- packages/api-kit/src/utils/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTxServiceBaseUrl = void 0;
-function getTxServiceBaseUrl(txServiceUrl) {
-    return `${txServiceUrl}/api`;
-}
-exports.getTxServiceBaseUrl = getTxServiceBaseUrl;
-//# sourceMappingURL=index.js.map

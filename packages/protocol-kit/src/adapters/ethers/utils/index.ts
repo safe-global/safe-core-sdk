cfe577b98fdb980deec40397271537a9b8205f5a
Raymond Feng <enjoyjava@gmail.com>
2023-04-02 08:52:01 -0700

    fix: relax signer compatible check
    instanceof operator does not work reliably as the Signer instance can be
    created from another ethers package in the dependency tree. and it causes
    the following error:
    
    TypeError: __classPrivateFieldGet(...).getNetwork is not a function
          at EthersAdapter.getChainId (/Users/rfeng/Projects/collabland/collabland-monorepo/components/ethereum/node_modules/@safe-global/safe-ethers-lib/src/EthersAdapter.ts:90:34)
          at ContractManager.init (/Users/rfeng/Projects/collabland/collabland-monorepo/components/ethereum/node_modules/@safe-global/safe-core-sdk/src/managers/contractManager.ts:39:38)
    
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- packages/protocol-kit/src/adapters/ethers/utils/index.ts
+++ packages/protocol-kit/src/adapters/ethers/utils/index.ts
@@ -1,6 +1,8 @@
 import { TypedDataSigner } from '@ethersproject/abstract-signer'
 import { ContractTransaction } from '@ethersproject/contracts'
 import { EthersTransactionOptions, EthersTransactionResult } from '../types'
+import { Signer } from '@ethersproject/abstract-signer'
+import { Provider } from '@ethersproject/providers'
 
 export function sameString(str1: string, str2: string): boolean {
   return str1.toLowerCase() === str2.toLowerCase()
@@ -19,4 +21,21 @@ export function toTxResult(
 
 export function isTypedDataSigner(signer: any): signer is TypedDataSigner {
   return (signer as unknown as TypedDataSigner)._signTypedData !== undefined
+}
+
+/**
+ * Check if the signerOrProvider is compatible with `Signer`
+ * @param signerOrProvider - Signer or provider
+ * @returns true if the parameter is compatible with `Signer`
+ */
+export function isSignerCompatible(
+  signerOrProvider: Signer | Provider
+): signerOrProvider is Signer {
+  const candidate = signerOrProvider as Signer
+  return (
+    (typeof candidate.signMessage === 'function' &&
+      typeof candidate.signTransaction === 'function' &&
+      candidate._isSigner) ||
+    candidate instanceof Signer
+  )
 }

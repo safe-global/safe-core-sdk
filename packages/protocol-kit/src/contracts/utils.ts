a567fed33c061ceebed4d2bca1e2a5d98d0c515e
Daniel Somoza <daniel.somoza@safe.global>
2023-05-10 13:01:49 +0200

    Renaming PredictedSafeProps type to PredictedSafeAddressProps

--- packages/protocol-kit/src/contracts/utils.ts
+++ packages/protocol-kit/src/contracts/utils.ts
@@ -20,7 +20,7 @@ import { SAFE_LAST_VERSION } from '@safe-global/protocol-kit/contracts/config'
 export const PREDETERMINED_SALT_NONCE =
   '0xb1073742015cbcf5a3a4d9d1ae33ecf619439710b89475f92e2abd2117e90f90'
 
-export interface PredictSafeProps {
+export interface PredictSafeAddressProps {
   ethAdapter: EthAdapter
   safeAccountConfig: SafeAccountConfig
   safeDeploymentConfig?: SafeDeploymentConfig
@@ -108,7 +108,7 @@ export async function predictSafeAddress({
   safeDeploymentConfig = {},
   isL1SafeMasterCopy = false,
   customContracts
-}: PredictSafeProps): Promise<string> {
+}: PredictSafeAddressProps): Promise<string> {
   validateSafeAccountConfig(safeAccountConfig)
   validateSafeDeploymentConfig(safeDeploymentConfig)
 

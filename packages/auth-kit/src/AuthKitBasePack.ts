f6deeda51b8ffe0be71b1e9923e6666cd8091720
Yago Pérez Vázquez <yago@safe.global>
2023-05-22 13:24:31 +0200

    doc authbasepack

--- packages/auth-kit/src/AuthKitBasePack.ts
+++ packages/auth-kit/src/AuthKitBasePack.ts
@@ -8,14 +8,54 @@ import type { AuthKitSignInData } from './types'
 export abstract class AuthKitBasePack {
   safeAuthData?: AuthKitSignInData
 
+  /**
+   * Initialize the pack
+   * @param options The provider specific options
+   */
   abstract init(options?: unknown): Promise<void>
+
+  /**
+   * Start the sign in flow in the pack
+   * @returns The sign in data from the provider
+   */
   abstract signIn(): Promise<AuthKitSignInData>
+
+  /**
+   * Start the sign out flow in the pack
+   */
   abstract signOut(): Promise<void>
+
+  /**
+   * Get the provider instance based on the pack
+   * @returns The provider instance
+   */
   abstract getProvider(): ethers.providers.ExternalProvider | null
+
+  /**
+   * Get the user info from the provider
+   * @returns The user info from the provider
+   */
   abstract getUserInfo(): Promise<unknown>
+
+  /**
+   * Subscribe to an event
+   * @param event  The event to subscribe to
+   * @param handler  The handler to be called when the event is triggered
+   */
   abstract subscribe(event: unknown, handler: unknown): void
+
+  /**
+   * Unsubscribe from an event
+   * @param event  The event to unsubscribe from
+   * @param handler The handler to be removed from the event
+   */
   abstract unsubscribe(event: unknown, handler: unknown): void
 
+  /**
+   * Get the list of Safe addresses owned by the user in the chain
+   * @param txServiceUrl The URL of the Safe Transaction Service
+   * @returns The list of Safe addresses owned by the user in the chain
+   */
   async getSafes(txServiceUrl: string): Promise<string[]> {
     const apiKit = this.#getApiKit(txServiceUrl)
 
@@ -29,6 +69,10 @@ export abstract class AuthKitBasePack {
     }
   }
 
+  /**
+   * Get the owner address from the provider
+   * @returns The signer address
+   */
   async getAddress(): Promise<string> {
     if (!this.getProvider()) {
       throw new Error('Provider is not defined')

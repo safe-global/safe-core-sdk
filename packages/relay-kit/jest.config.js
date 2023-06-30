d681e5ea0d0a81716a98fdb68876fbde039add67
Yago Pérez Vázquez <yago@safe.global>
2023-06-30 14:09:14 +0200

    REmove the jest settings config

--- packages/relay-kit/jest.config.js
+++ packages/relay-kit/jest.config.js
@@ -8,8 +8,7 @@ const config = {
     '^@safe-global/protocol-kit/typechain/(.*)$': '<rootDir>/../protocol-kit/typechain/$1',
     '^@safe-global/protocol-kit/(.*)$': '<rootDir>/../protocol-kit/src/$1',
     '^@safe-global/relay-kit/(.*)$': '<rootDir>/src/$1'
-  },
-  coverageReporters: ['html', ['lcovonly', { projectRoot: '../../' }], 'text-summary']
+  }
 }
 
 module.exports = config

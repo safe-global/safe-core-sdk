342b30c5a9e953ee1fee4cb2ca32669ccca4065b
Germán Martínez <6764315+germartinez@users.noreply.github.com>
2023-07-05 05:21:03 +0200

    Fix web3 contract instances

--- packages/protocol-kit/src/adapters/web3/contracts/contractInstancesWeb3.ts
+++ packages/protocol-kit/src/adapters/web3/contracts/contractInstancesWeb3.ts
@@ -11,34 +11,52 @@ import { Multi_send as MultiSend_V1_3_0 } from '@safe-global/protocol-kit/typech
 import { Multi_send_call_only as MultiSendCallOnly_V1_3_0 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.3.0/Multi_send_call_only'
 import { Proxy_factory as SafeProxyFactory_V1_3_0 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.3.0/Proxy_factory'
 import { Sign_message_lib as SignMessageLib_V1_3_0 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.3.0/Sign_message_lib'
+import { Compatibility_fallback_handler as CompatibilityFallbackHandler_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Compatibility_fallback_handler'
+import { Create_call as CreateCall_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Create_call'
+import { Multi_send as MultiSend_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Multi_send'
+import { Multi_send_call_only as MultiSendCallOnly_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Multi_send_call_only'
+import { Safe as SafeMasterCopy_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Safe'
+import { Safe_proxy_factory as SafeProxyFactory_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Safe_proxy_factory'
+import { Sign_message_lib as SignMessageLib_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Sign_message_lib'
 import { SafeVersion } from '@safe-global/safe-core-sdk-types'
 import CompatibilityFallbackHandler_V1_3_0_Web3 from './CompatibilityFallbackHandler/v1.3.0/CompatibilityFallbackHandler_V1_3_0_Web3'
+import CompatibilityFallbackHandler_V1_4_1_Web3 from './CompatibilityFallbackHandler/v1.4.1/CompatibilityFallbackHandler_V1_4_1_Web3'
 import CreateCallContract_V1_3_0_Web3 from './CreateCall/v1.3.0/CreateCallEthersContract_V1_3_0_Web3'
+import CreateCallContract_V1_4_1_Web3 from './CreateCall/v1.4.1/CreateCallEthersContract_V1_4_1_Web3'
 import MultiSendContract_V1_1_1_Web3 from './MultiSend/v1.1.1/MultiSendContract_V1_1_1_Web3'
 import MultiSendContract_V1_3_0_Web3 from './MultiSend/v1.3.0/MultiSendContract_V1_3_0_Web3'
+import MultiSendContract_V1_4_1_Web3 from './MultiSend/v1.4.1/MultiSendContract_V1_4_1_Web3'
 import MultiSendCallOnlyContract_V1_3_0_Web3 from './MultiSendCallOnly/v1.3.0/MultiSendCallOnlyContract_V1_3_0_Web3'
+import MultiSendCallOnlyContract_V1_4_1_Web3 from './MultiSendCallOnly/v1.4.1/MultiSendCallOnlyContract_V1_4_1_Web3'
 import SafeContract_V1_0_0_Web3 from './Safe/v1.0.0/SafeContract_V1_0_0_Web3'
 import SafeContract_V1_1_1_Web3 from './Safe/v1.1.1/SafeContract_V1_1_1_Web3'
 import SafeContract_V1_2_0_Web3 from './Safe/v1.2.0/SafeContract_V1_2_0_Web3'
 import SafeContract_V1_3_0_Web3 from './Safe/v1.3.0/SafeContract_V1_3_0_Web3'
+import SafeContract_V1_4_1_Web3 from './Safe/v1.4.1/SafeContract_V1_4_1_Web3'
 import SafeProxyFactoryContract_V1_0_0_Web3 from './SafeProxyFactory/v1.0.0/SafeProxyFactoryContract_V1_0_0_Web3'
 import SafeProxyFactoryContract_V1_1_1_Web3 from './SafeProxyFactory/v1.1.1/SafeProxyFactoryContract_V1_1_1_Web3'
 import SafeProxyFactoryContract_V1_3_0_Web3 from './SafeProxyFactory/v1.3.0/SafeProxyFactoryContract_V1_3_0_Web3'
+import SafeProxyFactoryContract_V1_4_1_Web3 from './SafeProxyFactory/v1.4.1/SafeProxyFactoryContract_V1_4_1_Web3'
 import SignMessageLibContract_V1_3_0_Web3 from './SignMessageLib/v1.3.0/SignMessageLibContract_V1_3_0_Web3'
+import SignMessageLibContract_V1_4_1_Web3 from './SignMessageLib/v1.4.1/SignMessageLibContract_V1_4_1_Web3'
 
 export function getSafeContractInstance(
   safeVersion: SafeVersion,
   safeContract:
+    | SafeMasterCopy_V1_4_1
     | SafeMasterCopy_V1_3_0
     | SafeMasterCopy_V1_2_0
     | SafeMasterCopy_V1_1_1
     | SafeMasterCopy_V1_0_0
 ):
+  | SafeContract_V1_4_1_Web3
   | SafeContract_V1_3_0_Web3
   | SafeContract_V1_2_0_Web3
   | SafeContract_V1_1_1_Web3
   | SafeContract_V1_0_0_Web3 {
   switch (safeVersion) {
+    case '1.4.1':
+      return new SafeContract_V1_4_1_Web3(safeContract as SafeMasterCopy_V1_4_1)
     case '1.3.0':
       return new SafeContract_V1_3_0_Web3(safeContract as SafeMasterCopy_V1_3_0)
     case '1.2.0':
@@ -54,9 +72,15 @@ export function getSafeContractInstance(
 
 export function getCompatibilityFallbackHandlerContractInstance(
   safeVersion: SafeVersion,
-  compatibilityFallbackhandlerContract: CompatibilityFallbackHandler_V1_3_0
-): CompatibilityFallbackHandler_V1_3_0_Web3 {
+  compatibilityFallbackhandlerContract:
+    | CompatibilityFallbackHandler_V1_4_1
+    | CompatibilityFallbackHandler_V1_3_0
+): CompatibilityFallbackHandler_V1_4_1_Web3 | CompatibilityFallbackHandler_V1_3_0_Web3 {
   switch (safeVersion) {
+    case '1.4.1':
+      return new CompatibilityFallbackHandler_V1_4_1_Web3(
+        compatibilityFallbackhandlerContract as CompatibilityFallbackHandler_V1_4_1
+      )
     case '1.3.0':
     case '1.2.0':
     case '1.1.1':
@@ -70,9 +94,11 @@ export function getCompatibilityFallbackHandlerContractInstance(
 
 export function getMultiSendContractInstance(
   safeVersion: SafeVersion,
-  multiSendContract: MultiSend_V1_3_0 | MultiSend_V1_1_1
-): MultiSendContract_V1_3_0_Web3 | MultiSendContract_V1_1_1_Web3 {
+  multiSendContract: MultiSend_V1_4_1 | MultiSend_V1_3_0 | MultiSend_V1_1_1
+): MultiSendContract_V1_4_1_Web3 | MultiSendContract_V1_3_0_Web3 | MultiSendContract_V1_1_1_Web3 {
   switch (safeVersion) {
+    case '1.4.1':
+      return new MultiSendContract_V1_4_1_Web3(multiSendContract as MultiSend_V1_4_1)
     case '1.3.0':
       return new MultiSendContract_V1_3_0_Web3(multiSendContract as MultiSend_V1_3_0)
     case '1.2.0':
@@ -86,9 +112,13 @@ export function getMultiSendContractInstance(
 
 export function getMultiSendCallOnlyContractInstance(
   safeVersion: SafeVersion,
-  multiSendCallOnlyContract: MultiSendCallOnly_V1_3_0
-): MultiSendCallOnlyContract_V1_3_0_Web3 {
+  multiSendCallOnlyContract: MultiSendCallOnly_V1_4_1 | MultiSendCallOnly_V1_3_0
+): MultiSendCallOnlyContract_V1_4_1_Web3 | MultiSendCallOnlyContract_V1_3_0_Web3 {
   switch (safeVersion) {
+    case '1.4.1':
+      return new MultiSendCallOnlyContract_V1_4_1_Web3(
+        multiSendCallOnlyContract as MultiSendCallOnly_V1_4_1
+      )
     case '1.3.0':
     case '1.2.0':
     case '1.1.1':
@@ -104,14 +134,20 @@ export function getMultiSendCallOnlyContractInstance(
 export function getSafeProxyFactoryContractInstance(
   safeVersion: SafeVersion,
   safeProxyFactoryContract:
+    | SafeProxyFactory_V1_4_1
     | SafeProxyFactory_V1_3_0
     | SafeProxyFactory_V1_1_1
-    | SafeProxyFactoryContract_V1_0_0_Web3
+    | SafeProxyFactory_V1_0_0
 ):
+  | SafeProxyFactoryContract_V1_4_1_Web3
   | SafeProxyFactoryContract_V1_3_0_Web3
   | SafeProxyFactoryContract_V1_1_1_Web3
   | SafeProxyFactoryContract_V1_0_0_Web3 {
   switch (safeVersion) {
+    case '1.4.1':
+      return new SafeProxyFactoryContract_V1_4_1_Web3(
+        safeProxyFactoryContract as SafeProxyFactory_V1_4_1
+      )
     case '1.3.0':
       return new SafeProxyFactoryContract_V1_3_0_Web3(
         safeProxyFactoryContract as SafeProxyFactory_V1_3_0
@@ -132,9 +168,11 @@ export function getSafeProxyFactoryContractInstance(
 
 export function getSignMessageLibContractInstance(
   safeVersion: SafeVersion,
-  signMessageLibContract: SignMessageLib_V1_3_0
-): SignMessageLibContract_V1_3_0_Web3 {
+  signMessageLibContract: SignMessageLib_V1_4_1 | SignMessageLib_V1_3_0
+): SignMessageLibContract_V1_4_1_Web3 | SignMessageLibContract_V1_3_0_Web3 {
   switch (safeVersion) {
+    case '1.4.1':
+      return new SignMessageLibContract_V1_4_1_Web3(signMessageLibContract as SignMessageLib_V1_4_1)
     case '1.3.0':
       return new SignMessageLibContract_V1_3_0_Web3(signMessageLibContract as SignMessageLib_V1_3_0)
     default:
@@ -144,9 +182,11 @@ export function getSignMessageLibContractInstance(
 
 export function getCreateCallContractInstance(
   safeVersion: SafeVersion,
-  createCallContract: CreateCall_V1_3_0
-): CreateCallContract_V1_3_0_Web3 {
+  createCallContract: CreateCall_V1_4_1 | CreateCall_V1_3_0
+): CreateCallContract_V1_4_1_Web3 | CreateCallContract_V1_3_0_Web3 {
   switch (safeVersion) {
+    case '1.4.1':
+      return new CreateCallContract_V1_4_1_Web3(createCallContract as CreateCall_V1_4_1)
     case '1.3.0':
     case '1.2.0':
     case '1.1.1':

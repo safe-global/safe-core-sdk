c8e8546461608b5f1f02fecaf507e25a3f643ad8
Daniel Somoza <daniel.somoza@safe.global>
2023-08-10 19:04:00 +0200

    fixed tests for all Safe versions

--- packages/protocol-kit/tests/e2e/getEncodedTransaction.test.ts
+++ packages/protocol-kit/tests/e2e/getEncodedTransaction.test.ts
@@ -1,11 +1,13 @@
 import Safe from '@safe-global/protocol-kit/index'
 import { SafeTransactionDataPartial } from '@safe-global/safe-core-sdk-types'
+import { safeVersionDeployed } from '@safe-global/protocol-kit/hardhat/deploy/deploy-contracts'
 import chai from 'chai'
 import { deployments, waffle } from 'hardhat'
 import { getContractNetworks } from './utils/setupContractNetworks'
 import { getSafeWithOwners } from './utils/setupContracts'
 import { getEthAdapter } from './utils/setupEthAdapter'
 import { getAccounts } from './utils/setupTestNetwork'
+import { itif } from './utils/helpers'
 
 describe('getEncodedTransaction', () => {
   const setupTests = deployments.createFixture(async ({ deployments }) => {
@@ -19,7 +21,7 @@ describe('getEncodedTransaction', () => {
     }
   })
 
-  it('should return a transaction encoded', async () => {
+  itif(safeVersionDeployed >= '1.3.0')('should return a transaction encoded', async () => {
     const { accounts, contractNetworks } = await setupTests()
     const [account1, account2] = accounts
 
@@ -46,6 +48,36 @@ describe('getEncodedTransaction', () => {
       .expect(encodedTransaction)
       .to.be.equal(
         '0x6a761202000000000000000000000000ffcf8fdee72ac11b5c542428b35eef5769c409f000000000000000000000000000000000000000000000000006f05b59d3b200000000000000000000000000000000000000000000000000000000000000000140000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000016000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+      )
+  })
+
+  itif(safeVersionDeployed <= '1.2.0')('should return a transaction encoded', async () => {
+    const { accounts, contractNetworks } = await setupTests()
+    const [account1, account2] = accounts
+
+    const safe = await getSafeWithOwners([account1.address])
+    const ethAdapter = await getEthAdapter(account1.signer)
+
+    const safeSdk = await Safe.create({
+      ethAdapter,
+      safeAddress: safe.address,
+      contractNetworks
+    })
+
+    const safeTransactionData: SafeTransactionDataPartial = {
+      to: account2.address,
+      value: '500000000000000000', // 0.5 ETH
+      data: '0x'
+    }
+
+    const transaction = await safeSdk.createTransaction({ safeTransactionData })
+
+    const encodedTransaction = await safeSdk.getEncodedTransaction(transaction)
+
+    chai
+      .expect(encodedTransaction)
+      .to.be.equal(
+        '0x6a761202000000000000000000000000ffcf8fdee72ac11b5c542428b35eef5769c409f000000000000000000000000000000000000000000000000006f05b59d3b200000000000000000000000000000000000000000000000000000000000000000140000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000052090000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000016000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
       )
   })
 

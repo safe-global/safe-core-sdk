3e3ccee974d93f118933545bbc56abf24c9b7902
Daniel Somoza <daniel.somoza@safe.global>
2023-08-16 12:42:49 +0200

    Account abstraction kit code cleanup

--- packages/account-abstraction-kit/src/types/index.ts
+++ packages/account-abstraction-kit/src/types/index.ts
@@ -1,10 +1,5 @@
 import { RelayPack } from '@safe-global/relay-kit'
 
-export enum OperationType {
-  Call,
-  DelegateCall
-}
-
 export interface AccountAbstractionConfig {
   relayPack: RelayPack
 }

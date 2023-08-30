582dbf843f4e875a4c63145e22cf7491c1e89dc5
Yago Pérez Vázquez <yago@safe.global>
2023-08-30 12:47:24 +0200

    Fix operation value

--- packages/protocol-kit/tests/unit/eip-712.test.ts
+++ packages/protocol-kit/tests/unit/eip-712.test.ts
@@ -1,4 +1,4 @@
-import { SafeTransactionData } from '@safe-global/safe-core-sdk-types'
+import { SafeTransactionData, OperationType } from '@safe-global/safe-core-sdk-types'
 import chai from 'chai'
 import {
   EIP712_DOMAIN,
@@ -12,7 +12,7 @@ const safeTransactionData: SafeTransactionData = {
   to: '0x000',
   value: '111',
   data: '0x222',
-  operation: 333,
+  operation: OperationType.DelegateCall,
   safeTxGas: '444',
   baseGas: '555',
   gasPrice: '666',

81fc7e6b94c306184eab7a40be7640f5287164fb
Yago Pérez Vázquez <yago@safe.global>
2023-09-05 17:49:13 +0200

    Configure vite example projects to work with local packages

--- packages/api-kit/src/utils/config.ts
+++ packages/api-kit/src/utils/config.ts
@@ -1,6 +1,6 @@
 export const TRANSACTION_SERVICE_URLS: Record<number, string> = {
   1: 'https://safe-transaction-mainnet.safe.global',
-  5: 'https://safe-transaction-goerli.safe.global/',
+  5: 'https://safe-transaction-goerli.safe.global',
   10: 'https://safe-transaction-optimism.safe.global',
   56: 'https://safe-transaction-bsc.safe.global',
   100: 'https://safe-transaction-gnosis-chain.safe.global',
@@ -9,6 +9,6 @@ export const TRANSACTION_SERVICE_URLS: Record<number, string> = {
   42161: 'https://safe-transaction-arbitrum.safe.global',
   42220: 'https://safe-transaction-celo.safe.global',
   43114: 'https://safe-transaction-avalanche.safe.global',
-  84531: 'https://safe-transaction-base-testnet.safe.global/',
+  84531: 'https://safe-transaction-base-testnet.safe.global',
   1313161554: 'https://safe-transaction-aurora.safe.global'
 }
--- packages/auth-kit/example/vite.config.ts
+++ packages/auth-kit/example/vite.config.ts
@@ -11,7 +11,11 @@ export default defineConfig({
   },
   optimizeDeps: {
     disabled: false,
-    include: ['@safe-global/protocol-kit', '@safe-global/api-kit'],
+    include: [
+      '@safe-global/protocol-kit',
+      '@safe-global/api-kit',
+      '@safe-global/safe-core-sdk-types'
+    ],
     esbuildOptions: {
       define: {
         global: 'globalThis'
@@ -31,6 +35,14 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      // Map the packages to the local build directories. Rebuild the project and restart the server if any package change locally
+      '@safe-global/api-kit': path.resolve(__dirname, '../../api-kit/dist/src'),
+      '@safe-global/protocol-kit': path.resolve(__dirname, '../../protocol-kit/dist/src'),
+      '@safe-global/safe-core-sdk-types': path.resolve(
+        __dirname,
+        '../../safe-core-sdk-types/dist/src'
+      ),
+      // Map this kit to the local source directory. Any changes to the source files are reflected on the fly
       '@safe-global/auth-kit': path.resolve(__dirname, '../src')
     }
   }

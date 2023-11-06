ae65c693901676b7e39c8803dd47be3f2600ca3e
Yago Pérez Vázquez <yago@safe.global>
2023-11-06 10:04:16 +0100

    Some comments

--- packages/auth-kit/src/packs/safe-auth/SafeAuthPack.ts
+++ packages/auth-kit/src/packs/safe-auth/SafeAuthPack.ts
@@ -36,8 +36,8 @@ export class SafeAuthPack extends AuthKitBasePack {
   }
 
   /**
-   * Returns ig the user is already authenticated
-   * The user is authenticated when the communicationProvider is available
+   * Check if the user is authenticated
+   * Checking the communication provider for this information
    */
   get isAuthenticated(): boolean {
     return this.sdk.communicationProvider.isLoggedIn
@@ -46,7 +46,7 @@ export class SafeAuthPack extends AuthKitBasePack {
   /**
    * Initialize the SafeAuthPack
    * @param options The options to initialize the SafeAuthPack
-   * @throws Error if there was an error initializing the SafeAuthPack
+   * @throws Error if there was an error initializing the Web3Auth SDK
    */
   async init(options: SafeAuthInitOptions) {
     try {
@@ -63,6 +63,7 @@ export class SafeAuthPack extends AuthKitBasePack {
   /**
    * Connect to the Web3Auth services and login
    * @param options The options to connect to the Web3Auth services
+   * When loginProvider is specified the native provider authentication will be used instead the provider selector UI modal
    * @returns An AuthKitSignInData object with the signer address and the associated safes
    */
   async signIn(options?: SafeAuthSignInOptions): Promise<AuthKitSignInData> {
@@ -75,17 +76,12 @@ export class SafeAuthPack extends AuthKitBasePack {
     const eoa = await this.getAddress()
     const safes = await this.getSafes(this.#config?.txServiceUrl || '')
 
-    const signInData = {
-      eoa,
-      safes
-    }
-
-    return signInData
+    return { eoa, safes }
   }
 
   /**
-   * Get the provider if available
-   * @returns A EIP-1193 compatible provider
+   * Get the provider returned by the Web3Auth SDK
+   * @returns A EIP-1193 compatible provider. Can be wrapped with ethers or web3
    */
   getProvider(): ExternalProvider | null {
     return this.#provider
@@ -93,6 +89,9 @@ export class SafeAuthPack extends AuthKitBasePack {
 
   /**
    * Disconnect from the Web3Auth services and logout
+   * Use reset parameter to true when you want to remove completely the iframe.
+   * When this is false you can logout and login again without the need to refresh the page.
+   * You need to re-instantiate the pack if you reset completely it
    * @param options The options to disconnect from the Web3Auth services
    */
   async signOut(options?: SafeAuthSignOutOptions) {
@@ -110,8 +109,9 @@ export class SafeAuthPack extends AuthKitBasePack {
   }
 
   /**
-   * Get user information once authenticated
-   * @returns The specific SafeAuthUserInfo
+   * Get user information. Use it after authentication
+   * @returns The specific user information coming from the oAuth or email provider
+   * @throws Error if there was an error initializing the Web3Auth SDK
    */
   async getUserInfo(): Promise<SafeAuthUserInfo> {
     if (!this.sdk) {
@@ -124,7 +124,10 @@ export class SafeAuthPack extends AuthKitBasePack {
   }
 
   /**
-   * Subscribe to the events
+   * Subscribe to events (accountsChanged, chainChanged)
+   * You can use the accountsChanged event to check the accounts and
+   * as an indicator that a first authentication happened, so if the page
+   * is refreshed you can call the signIn method immediately
    * @param event The event you want to subscribe to
    * @param handler The event handler
    */

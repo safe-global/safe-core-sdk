3be781d69fd5776aedcb46ed8bdfd4e4848de051
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2023-12-07 15:34:48 +0100

    Various fixes in `SafeContract_v1_3_0_Web3`

--- packages/protocol-kit/src/adapters/web3/contracts/Safe/v1.3.0/SafeContract_v1_3_0_Web3.ts
+++ packages/protocol-kit/src/adapters/web3/contracts/Safe/v1.3.0/SafeContract_v1_3_0_Web3.ts
@@ -64,26 +64,12 @@ class SafeContract_v1_3_0_Web3
     this.safeVersion = safeVersion
   }
 
-  encode: EncodeSafeFunction<SafeContract_v1_3_0_Abi> = (functionToEncode, args) => {
-    return this.contract.methods[functionToEncode](...args).encodeABI()
+  async VERSION(): Promise<[SafeVersion]> {
+    return [await this.contract.methods.VERSION().call()]
   }
 
-  estimateGas: EstimateGasSafeFunction<SafeContract_v1_3_0_Abi, Web3TransactionOptions> = (
-    functionToEstimate,
-    args,
-    options = {}
-  ) => {
-    return this.contract.methods[functionToEstimate](...args)
-      .estimateGas(options)
-      .then(BigInt)
-  }
-
-  async VERSION(): Promise<[string]> {
-    return [await this.contract.methods.VERSION()]
-  }
-
-  async approvedHashes([owner, txHash]: readonly [string, string]): Promise<[bigint]> {
-    return [await this.contract.methods.approvedHashes(owner, txHash)]
+  async approvedHashes(args: readonly [owner: string, txHash: string]): Promise<[bigint]> {
+    return [await this.contract.methods.approvedHashes(...args).call()]
   }
 
   async checkNSignatures(
@@ -91,7 +77,7 @@ class SafeContract_v1_3_0_Web3
   ): Promise<[]> {
     // this method just checks whether the signature provided is valid for the provided data and hash. Reverts otherwise.
     if (this.contract.methods.checkNSignatures) {
-      await this.contract.methods.checkNSignatures(...args)
+      await this.contract.methods.checkNSignatures(...args).call()
     }
     return []
   }
@@ -99,12 +85,12 @@ class SafeContract_v1_3_0_Web3
   async checkSignatures(
     args: readonly [dataHash: string, data: string, signatures: string]
   ): Promise<[]> {
-    await this.contract.methods.checkSignatures(...args)
+    await this.contract.methods.checkSignatures(...args).call()
     return []
   }
 
   async domainSeparator(): Promise<[string]> {
-    return [await this.contract.methods.domainSeparator()]
+    return [await this.contract.methods.domainSeparator().call()]
   }
 
   async encodeTransactionData(
@@ -121,39 +107,29 @@ class SafeContract_v1_3_0_Web3
       _nonce: bigint
     ]
   ): Promise<[string]> {
-    return [await this.contract.methods.encodeTransactionData(...args)]
+    return [await this.contract.methods.encodeTransactionData(...args).call()]
   }
 
   async getChainId(): Promise<[bigint]> {
-    return [await this.contract.methods.getChainId()]
-  }
-
-  // TODO: review this custom method
-  async getModules(): Promise<string[]> {
-    const { array } = await this.contract.methods.getModulesPaginated(SENTINEL_ADDRESS, 10).call()
-    return array
+    return [await this.contract.methods.getChainId().call()]
   }
 
   getModulesPaginated(
     args: readonly [start: string, pageSize: bigint]
   ): Promise<[modules: string[], next: string]> {
-    return this.contract.methods.getModulesPaginated(...args)
-  }
-
-  async isModuleEnabled(args: readonly [moduleAddress: string]): Promise<[boolean]> {
-    return [await this.contract.methods.isModuleEnabled(...args)]
+    return this.contract.methods.getModulesPaginated(...args).call()
   }
 
   async getOwners(): Promise<readonly [string[]]> {
-    return [await this.contract.methods.getOwners()]
+    return [await this.contract.methods.getOwners().call()]
   }
 
   async getStorageAt(args: readonly [offset: bigint, length: bigint]): Promise<[string]> {
-    return [await this.contract.methods.getStorageAt(...args)]
+    return [await this.contract.methods.getStorageAt(...args).call()]
   }
 
   async getThreshold(): Promise<[bigint]> {
-    return [await this.contract.methods.getThreshold()]
+    return [await this.contract.methods.getThreshold().call()]
   }
 
   async getTransactionHash(
@@ -170,34 +146,59 @@ class SafeContract_v1_3_0_Web3
       _nonce: bigint
     ]
   ): Promise<[string]> {
-    return [await this.contract.methods.getTransactionHash(...args)]
+    return [await this.contract.methods.getTransactionHash(...args).call()]
+  }
+
+  async isModuleEnabled(args: readonly [moduleAddress: string]): Promise<[boolean]> {
+    return [await this.contract.methods.isModuleEnabled(...args).call()]
   }
 
   async isOwner(args: readonly [address: string]): Promise<[boolean]> {
-    return [await this.contract.methods.isOwner(...args)]
+    return [await this.contract.methods.isOwner(...args).call()]
   }
 
   async nonce(): Promise<[bigint]> {
-    return [await this.contract.methods.nonce()]
-  }
-
-  async getNonce(): Promise<number> {
-    return Number(await this.contract.methods.nonce().call())
+    return [await this.contract.methods.nonce().call()]
   }
 
   async signedMessages(args: readonly [messageHash: string]): Promise<[bigint]> {
-    return [await this.contract.methods.signedMessages(...args)]
+    return [await this.contract.methods.signedMessages(...args).call()]
   }
 
-  // custom methods (not defined in the Safe Contract)
-  async isValidTransaction(
+  encode: EncodeSafeFunction<SafeContract_v1_3_0_Abi> = (functionToEncode, args) => {
+    return this.contract.methods[functionToEncode](...args).encodeABI()
+  }
+
+  estimateGas: EstimateGasSafeFunction<SafeContract_v1_3_0_Abi, Web3TransactionOptions> = (
+    functionToEstimate,
+    args,
+    options = {}
+  ) => {
+    return this.contract.methods[functionToEstimate](...args)
+      .estimateGas(options)
+      .then(BigInt)
+  }
+
+  // Custom method (not defined in the Safe Contract)
+  // TODO: review this custom method
+  async getModules(): Promise<string[]> {
+    const { array } = await this.contract.methods.getModulesPaginated(SENTINEL_ADDRESS, 10).call()
+    return array
+  }
+
+  // Custom method (not defined in the Safe Contract)
+  getAddress(): Promise<string> {
+    return Promise.resolve(this.contract.options.address)
+  }
+
+  // Custom method (not defined in the Safe Contract)
+  async execTransaction(
     safeTransaction: SafeTransaction,
     options?: Web3TransactionOptions
-  ): Promise<boolean> {
-    let isTxValid = false
-    try {
-      if (options && !options.gas) {
-        options.gas = await this.estimateGas(
+  ): Promise<Web3TransactionResult> {
+    if (options && !options.gas) {
+      options.gas = (
+        await this.estimateGas(
           'execTransaction',
           [
             safeTransaction.data.to,
@@ -212,50 +213,7 @@ class SafeContract_v1_3_0_Web3
             safeTransaction.encodedSignatures()
           ],
           options
-        ).toString()
-      }
-      isTxValid = await this.contract.methods
-        .execTransaction(
-          safeTransaction.data.to,
-          BigInt(safeTransaction.data.value),
-          safeTransaction.data.data,
-          safeTransaction.data.operation,
-          BigInt(safeTransaction.data.safeTxGas),
-          BigInt(safeTransaction.data.baseGas),
-          BigInt(safeTransaction.data.gasPrice),
-          safeTransaction.data.gasToken,
-          safeTransaction.data.refundReceiver,
-          safeTransaction.encodedSignatures()
         )
-        .call(options)
-    } catch {}
-    return isTxValid
-  }
-
-  getAddress(): Promise<string> {
-    return Promise.resolve(this.contract.options.address)
-  }
-
-  async execTransaction(
-    safeTransaction: SafeTransaction,
-    options?: Web3TransactionOptions
-  ): Promise<Web3TransactionResult> {
-    if (options && !options.gas) {
-      options.gas = await this.estimateGas(
-        'execTransaction',
-        [
-          safeTransaction.data.to,
-          BigInt(safeTransaction.data.value),
-          safeTransaction.data.data,
-          safeTransaction.data.operation,
-          BigInt(safeTransaction.data.safeTxGas),
-          BigInt(safeTransaction.data.baseGas),
-          BigInt(safeTransaction.data.gasPrice),
-          safeTransaction.data.gasToken,
-          safeTransaction.data.refundReceiver,
-          safeTransaction.encodedSignatures()
-        ],
-        options
       ).toString()
     }
     const txResponse = this.contract.methods
@@ -276,12 +234,58 @@ class SafeContract_v1_3_0_Web3
     return toTxResult(txResponse, options)
   }
 
+  // Custom method (not defined in the Safe Contract)
+  async isValidTransaction(
+    safeTransaction: SafeTransaction,
+    options?: Web3TransactionOptions
+  ): Promise<boolean> {
+    let isTxValid = false
+    try {
+      if (options && !options.gas) {
+        options.gas = (
+          await this.estimateGas(
+            'execTransaction',
+            [
+              safeTransaction.data.to,
+              BigInt(safeTransaction.data.value),
+              safeTransaction.data.data,
+              safeTransaction.data.operation,
+              BigInt(safeTransaction.data.safeTxGas),
+              BigInt(safeTransaction.data.baseGas),
+              BigInt(safeTransaction.data.gasPrice),
+              safeTransaction.data.gasToken,
+              safeTransaction.data.refundReceiver,
+              safeTransaction.encodedSignatures()
+            ],
+            options
+          )
+        ).toString()
+      }
+      isTxValid = await this.contract.methods
+        .execTransaction(
+          safeTransaction.data.to,
+          BigInt(safeTransaction.data.value),
+          safeTransaction.data.data,
+          safeTransaction.data.operation,
+          BigInt(safeTransaction.data.safeTxGas),
+          BigInt(safeTransaction.data.baseGas),
+          BigInt(safeTransaction.data.gasPrice),
+          safeTransaction.data.gasToken,
+          safeTransaction.data.refundReceiver,
+          safeTransaction.encodedSignatures()
+        )
+        .call(options)
+    } catch {}
+    return isTxValid
+  }
+
+  // Custom method (not defined in the Safe Contract)
   async approveHash(
     hash: string,
     options?: Web3TransactionOptions
   ): Promise<Web3TransactionResult> {
     if (options && !options.gas) {
-      options.gas = await this.estimateGas('approveHash', [hash], { ...options }).toString()
+      options.gas = (await this.estimateGas('approveHash', [hash], { ...options })).toString()
     }
     const txResponse = this.contract.methods.approveHash(hash).send(options)
     return toTxResult(txResponse, options)

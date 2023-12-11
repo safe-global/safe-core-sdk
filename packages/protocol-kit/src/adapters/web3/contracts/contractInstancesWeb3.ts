1a5cb2a813f0c543aa6c6e1300c260ca79b9b177
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2023-12-11 17:55:50 +0100

    Update `contractInstancesWeb3.ts` to use the new class

--- packages/protocol-kit/src/adapters/web3/contracts/contractInstancesWeb3.ts
+++ packages/protocol-kit/src/adapters/web3/contracts/contractInstancesWeb3.ts
@@ -1,7 +1,9 @@
 import { AbiItem } from 'web3-utils'
 import SafeContract_v1_3_0_Web3 from '@safe-global/protocol-kit/adapters/web3/contracts/Safe/v1.3.0/SafeContract_v1_3_0_Web3'
+import SafeContract_v1_4_1_Web3 from '@safe-global/protocol-kit/adapters/web3/contracts/Safe/v1.4.1/SafeContract_v1_4_1_Web3'
 import Web3Adapter from '@safe-global/protocol-kit/adapters/web3/Web3Adapter'
 import { SafeContract_v1_3_0_Abi } from '@safe-global/protocol-kit/contracts/AbiType/Safe/v1.3.0/SafeContract_v1_3_0'
+import { SafeContract_v1_4_1_Abi } from '@safe-global/protocol-kit/contracts/AbiType/Safe/v1.4.1/SafeContract_v1_4_1'
 import { Gnosis_safe as SafeSingleton_V1_0_0 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.0.0/Gnosis_safe'
 import { Proxy_factory as SafeProxyFactory_V1_0_0 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.0.0/Proxy_factory'
 import { Gnosis_safe as SafeSingleton_V1_1_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.1.1/Gnosis_safe'
@@ -19,7 +21,6 @@ import { Compatibility_fallback_handler as CompatibilityFallbackHandler_V1_4_1 }
 import { Create_call as CreateCall_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Create_call'
 import { Multi_send as MultiSend_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Multi_send'
 import { Multi_send_call_only as MultiSendCallOnly_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Multi_send_call_only'
-import { Safe as SafeSingleton_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Safe'
 import { Safe_proxy_factory as SafeProxyFactory_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Safe_proxy_factory'
 import { Sign_message_lib as SignMessageLib_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Sign_message_lib'
 import { Simulate_tx_accessor as SimulateTxAccessor_V1_4_1 } from '@safe-global/protocol-kit/typechain/src/web3-v1/v1.4.1/Simulate_tx_accessor'
@@ -36,7 +37,6 @@ import MultiSendCallOnlyContract_V1_4_1_Web3 from './MultiSendCallOnly/v1.4.1/Mu
 import SafeContract_V1_0_0_Web3 from './Safe/v1.0.0/SafeContract_V1_0_0_Web3'
 import SafeContract_V1_1_1_Web3 from './Safe/v1.1.1/SafeContract_V1_1_1_Web3'
 import SafeContract_V1_2_0_Web3 from './Safe/v1.2.0/SafeContract_V1_2_0_Web3'
-import SafeContract_V1_4_1_Web3 from './Safe/v1.4.1/SafeContract_V1_4_1_Web3_OLD'
 import SafeProxyFactoryContract_V1_0_0_Web3 from './SafeProxyFactory/v1.0.0/SafeProxyFactoryContract_V1_0_0_Web3'
 import SafeProxyFactoryContract_V1_1_1_Web3 from './SafeProxyFactory/v1.1.1/SafeProxyFactoryContract_V1_1_1_Web3'
 import SafeProxyFactoryContract_V1_3_0_Web3 from './SafeProxyFactory/v1.3.0/SafeProxyFactoryContract_V1_3_0_Web3'
@@ -48,27 +48,27 @@ import SimulateTxAccessorContract_V1_4_1_Web3 from './SimulateTxAccessor/v1.4.1/
 
 export async function getSafeContractInstance(
   safeVersion: SafeVersion,
-  safeSingletonContract:
-    | SafeSingleton_V1_4_1
-    | SafeSingleton_V1_2_0
-    | SafeSingleton_V1_1_1
-    | SafeSingleton_V1_0_0,
+  safeSingletonContract: SafeSingleton_V1_2_0 | SafeSingleton_V1_1_1 | SafeSingleton_V1_0_0,
   contractAddress: string,
   web3Adapter: Web3Adapter,
   customContractAbi?: AbiItem | AbiItem[] | undefined,
   isL1SafeSingleton?: boolean
-): Promise<
-  | SafeContract_V1_4_1_Web3
-  | SafeContract_V1_2_0_Web3
-  | SafeContract_V1_1_1_Web3
-  | SafeContract_V1_0_0_Web3
-> {
+): Promise<SafeContract_V1_2_0_Web3 | SafeContract_V1_1_1_Web3 | SafeContract_V1_0_0_Web3> {
+  const chainId = await web3Adapter.getChainId()
   let safeContract
   switch (safeVersion) {
     case '1.4.1':
-      return new SafeContract_V1_4_1_Web3(safeSingletonContract as SafeSingleton_V1_4_1)
+      safeContract = new SafeContract_v1_4_1_Web3(
+        chainId,
+        web3Adapter,
+        isL1SafeSingleton,
+        contractAddress,
+        // TODO: Remove this unknown after remove Typechain
+        customContractAbi as unknown as SafeContract_v1_4_1_Abi
+      )
+      // TODO: Remove this mapper after remove typechain
+      return safeContract.mapToTypechainContract()
     case '1.3.0':
-      const chainId = await web3Adapter.getChainId()
       safeContract = new SafeContract_v1_3_0_Web3(
         chainId,
         web3Adapter,

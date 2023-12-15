ad49269f497cc354c7ad261ddb032670bcc273c9
Yago Pérez Vázquez <yago@safe.global>
2023-12-15 13:26:57 +0100

    fix(onramp-kit): Some improvement to Monerium PR (#639)

--- packages/onramp-kit/example/client/src/components/monerium/Monerium.tsx
+++ packages/onramp-kit/example/client/src/components/monerium/Monerium.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { ethers } from 'ethers'
-import { AuthContext, Currency, OrderState, PaymentStandard } from '@monerium/sdk'
+import { AuthContext, OrderState, PaymentStandard } from '@monerium/sdk'
 import { Box } from '@mui/material'
 import Safe, { EthersAdapter } from '@safe-global/protocol-kit'
 
@@ -112,7 +112,6 @@ function Monerium() {
   const transfer = async (iban: string, amount: string) => {
     const tx = await moneriumClient?.send({
       amount,
-      currency: Currency.eur,
       counterpart: {
         identifier: {
           standard: 'iban' as PaymentStandard.iban,

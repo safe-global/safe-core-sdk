a6b0ccc33391af5e7751f803f213b2c953879519
farreldarian <farreldarian@users.noreply.github.com>
2024-01-14 18:06:33 +0700

    Initialize viem adapter

---
+++ packages/protocol-kit/src/adapters/viem/ViemAdapter.ts
@@ -0,0 +1,195 @@
+import { Account, Address, BlockTag, PublicClient, getAddress, isAddress } from 'viem'
+import { validateEip3770Address } from '../..'
+import {
+  CompatibilityFallbackHandlerContract,
+  CreateCallContract,
+  Eip3770Address,
+  EthAdapter,
+  EthAdapterTransaction,
+  GetContractProps,
+  MultiSendCallOnlyContract,
+  MultiSendContract,
+  SafeContract,
+  SafeProxyFactoryContract,
+  SafeTransactionEIP712Args,
+  SignMessageLibContract,
+  SimulateTxAccessorContract
+} from '@safe-global/safe-core-sdk-types'
+import { getSafeContractInstance } from './contracts/contractInstancesViem'
+import { WalletClient } from 'viem'
+import { Chain } from 'viem'
+import { Transport } from 'viem'
+
+export class ViemAdapter implements EthAdapter {
+  constructor(
+    public readonly config: {
+      client: {
+        public: PublicClient<Transport, Chain>
+        wallet: WalletClient<Transport, Chain, Account>
+      }
+    }
+  ) {}
+
+  get client() {
+    return this.config.client
+  }
+
+  isAddress = isAddress
+
+  async getEip3770Address(fullAddress: string): Promise<Eip3770Address> {
+    const chainId = await this.getChainId()
+    return validateEip3770Address(fullAddress, BigInt(chainId))
+  }
+
+  getBalance(address: Address, defaultBlock?: BlockTag | undefined): Promise<bigint> {
+    return this.client.public.getBalance({
+      address,
+      blockTag: defaultBlock
+    })
+  }
+
+  getNonce(address: Address, defaultBlock?: BlockTag | undefined): Promise<number> {
+    return this.client.public.getTransactionCount({
+      address,
+      blockTag: defaultBlock
+    })
+  }
+
+  getChainId(): Promise<bigint> {
+    return this.client.public.getChainId().then(BigInt)
+  }
+
+  getChecksummedAddress = getAddress
+
+  async getSafeContract({
+    safeVersion,
+    singletonDeployment,
+    customContractAddress
+  }: GetContractProps): Promise<SafeContract> {
+    const chainId = await this.getChainId()
+    const contractAddress =
+      customContractAddress ?? singletonDeployment?.networkAddresses[chainId.toString()]
+    if (contractAddress == null) {
+      throw new Error('Invalid SafeProxy contract address')
+    }
+    return getSafeContractInstance(safeVersion, contractAddress as Address, this.client)
+  }
+
+  getMultiSendContract({
+    safeVersion,
+    singletonDeployment,
+    customContractAddress,
+    customContractAbi
+  }: GetContractProps): Promise<MultiSendContract> {
+    throw new Error('Method not implemented.')
+  }
+
+  getMultiSendCallOnlyContract({
+    safeVersion,
+    singletonDeployment,
+    customContractAddress,
+    customContractAbi
+  }: GetContractProps): Promise<MultiSendCallOnlyContract> {
+    throw new Error('Method not implemented.')
+  }
+
+  getCompatibilityFallbackHandlerContract({
+    safeVersion,
+    singletonDeployment,
+    customContractAddress,
+    customContractAbi
+  }: GetContractProps): Promise<CompatibilityFallbackHandlerContract> {
+    throw new Error('Method not implemented.')
+  }
+  getSafeProxyFactoryContract({
+    safeVersion,
+    singletonDeployment,
+    customContractAddress,
+    customContractAbi
+  }: GetContractProps): Promise<SafeProxyFactoryContract> {
+    throw new Error('Method not implemented.')
+  }
+
+  getSignMessageLibContract({
+    safeVersion,
+    singletonDeployment,
+    customContractAddress,
+    customContractAbi
+  }: GetContractProps): Promise<SignMessageLibContract> {
+    throw new Error('Method not implemented.')
+  }
+
+  getCreateCallContract({
+    safeVersion,
+    singletonDeployment,
+    customContractAddress,
+    customContractAbi
+  }: GetContractProps): Promise<CreateCallContract> {
+    throw new Error('Method not implemented.')
+  }
+
+  getSimulateTxAccessorContract({
+    safeVersion,
+    singletonDeployment,
+    customContractAddress,
+    customContractAbi
+  }: GetContractProps): Promise<SimulateTxAccessorContract> {
+    throw new Error('Method not implemented.')
+  }
+
+  getContractCode(address: string, defaultBlock?: string | number | undefined): Promise<string> {
+    throw new Error('Method not implemented.')
+  }
+
+  isContractDeployed(
+    address: string,
+    defaultBlock?: string | number | undefined
+  ): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+
+  getStorageAt(address: string, position: string): Promise<string> {
+    throw new Error('Method not implemented.')
+  }
+
+  getTransaction(transactionHash: string): Promise<any> {
+    throw new Error('Method not implemented.')
+  }
+
+  getSignerAddress(): Promise<string | undefined> {
+    throw new Error('Method not implemented.')
+  }
+
+  signMessage(message: string): Promise<string> {
+    throw new Error('Method not implemented.')
+  }
+
+  signTypedData(
+    safeTransactionEIP712Args: SafeTransactionEIP712Args,
+    signTypedDataVersion?: string | undefined
+  ): Promise<string> {
+    throw new Error('Method not implemented.')
+  }
+
+  estimateGas(
+    transaction: EthAdapterTransaction,
+    callback?: ((error: Error, gas: number) => void) | undefined
+  ): Promise<string> {
+    throw new Error('Method not implemented.')
+  }
+
+  call(
+    transaction: EthAdapterTransaction,
+    defaultBlock?: string | number | undefined
+  ): Promise<string> {
+    throw new Error('Method not implemented.')
+  }
+
+  encodeParameters(types: string[], values: any[]): string {
+    throw new Error('Method not implemented.')
+  }
+
+  decodeParameters(types: any[], values: string): { [key: string]: any } {
+    throw new Error('Method not implemented.')
+  }
+}

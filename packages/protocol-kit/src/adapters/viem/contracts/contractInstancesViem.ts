e4b5adf744d039f507c5e21990ec05f99d7dee1c
farreldarian <farreldarian@users.noreply.github.com>
2024-01-14 18:02:05 +0700

    Create getSafeContractInstance

---
+++ packages/protocol-kit/src/adapters/viem/contracts/contractInstancesViem.ts
@@ -0,0 +1,31 @@
+import { SafeVersion } from '@safe-global/safe-core-sdk-types'
+import { Account, Address, Chain, PublicClient, Transport, WalletClient } from 'viem'
+import SafeContract_V1_4_1_Viem from './Safe/v1.4.1/SafeContract_V1_4_1_Viem'
+import SafeContract_V1_3_0_Viem from './Safe/v1.3.0/SafeContract_V1_3_0_Viem'
+import SafeContract_V1_2_0_Viem from './Safe/v1.2.0/SafeContract_V1_2_0_Viem'
+import SafeContract_V1_1_1_Viem from './Safe/v1.1.1/SafeContract_V1_1_1_Viem'
+import SafeContract_V1_0_0_Viem from './Safe/v1.0.0/SafeContract_V1_0_0_Viem'
+
+export function getSafeContractInstance(
+  safeVersion: SafeVersion,
+  address: Address,
+  client: {
+    public: PublicClient<Transport, Chain>
+    wallet: WalletClient<Transport, Chain, Account>
+  }
+) {
+  switch (safeVersion) {
+    case '1.4.1':
+      return new SafeContract_V1_4_1_Viem({ address, client })
+    case '1.3.0':
+      return new SafeContract_V1_3_0_Viem({ address, client })
+    case '1.2.0':
+      return new SafeContract_V1_2_0_Viem({ address, client })
+    case '1.1.1':
+      return new SafeContract_V1_1_1_Viem({ address, client })
+    case '1.0.0':
+      return new SafeContract_V1_0_0_Viem({ address, client })
+    default:
+      throw new Error('Invalid Safe version')
+  }
+}

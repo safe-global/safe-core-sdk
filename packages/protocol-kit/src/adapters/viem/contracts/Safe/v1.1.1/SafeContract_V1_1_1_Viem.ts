56cd73b4d2293a7a5aab47c450040ba76d0e2d5d
farreldarian <farreldarian@users.noreply.github.com>
2024-01-14 17:55:20 +0700

    Create adapter for Safe 1.1.1

---
+++ packages/protocol-kit/src/adapters/viem/contracts/Safe/v1.1.1/SafeContract_V1_1_1_Viem.ts
@@ -0,0 +1,61 @@
+import { EMPTY_DATA, ZERO_ADDRESS } from '@safe-global/protocol-kit/adapters/ethers/utils/constants'
+import { Gnosis_safe__factory as Safe__factory } from '@safe-global/protocol-kit/typechain/src/ethers-v6/v1.1.1'
+import {
+  SafeSetupConfig,
+  TransactionOptions,
+  TransactionResult
+} from '@safe-global/safe-core-sdk-types'
+import SafeContractViem, { SafeContractViemBaseArgs } from '../SafeContractViem'
+import { Address, Hash, isAddressEqual } from 'viem'
+
+class SafeContract_V1_1_1_Viem extends SafeContractViem {
+  constructor(args: SafeContractViemBaseArgs) {
+    super({ ...args, abi: Safe__factory.abi })
+  }
+
+  async setup(
+    setupConfig: SafeSetupConfig,
+    options?: TransactionOptions
+  ): Promise<TransactionResult> {
+    const {
+      owners,
+      threshold,
+      to = ZERO_ADDRESS,
+      data = EMPTY_DATA,
+      fallbackHandler = ZERO_ADDRESS,
+      paymentToken = ZERO_ADDRESS,
+      payment = 0,
+      paymentReceiver = ZERO_ADDRESS
+    } = setupConfig
+
+    const txHash = await this.contract.write.setup(
+      [
+        owners as Address[],
+        BigInt(threshold),
+        to as Address,
+        data as Hash,
+        fallbackHandler as Hash,
+        paymentToken as Address,
+        BigInt(payment),
+        paymentReceiver as Address
+      ],
+      this.formatViemTransactionOptions(options ?? {})
+    )
+
+    return this.formatTransactionResult(txHash, options)
+  }
+
+  async getModules() {
+    return this.contract.read.getModules().then((res) => res as Address[])
+  }
+
+  async isModuleEnabled(moduleAddress: string): Promise<boolean> {
+    const modules = await this.getModules()
+    const isModuleEnabled = modules.some((enabledModuleAddress: Address) =>
+      isAddressEqual(enabledModuleAddress, moduleAddress as Address)
+    )
+    return isModuleEnabled
+  }
+}
+
+export default SafeContract_V1_1_1_Viem

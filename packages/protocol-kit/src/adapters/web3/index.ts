e1ecefbddc311c57a0b11e4e601068b61e5b29b4
Yago Pérez Vázquez <yago@safe.global>
2024-01-15 10:57:14 +0100

    Update adapter types

--- packages/protocol-kit/src/adapters/web3/index.ts
+++ packages/protocol-kit/src/adapters/web3/index.ts
@@ -1,7 +1,7 @@
 import Web3Adapter, { Web3AdapterConfig } from './Web3Adapter'
 import CreateCallWeb3Contract from './contracts/CreateCall/CreateCallWeb3Contract'
-import MultiSendWeb3Contract from './contracts/MultiSend/MultiSendWeb3Contract'
-import MultiSendCallOnlyWeb3Contract from './contracts/MultiSendCallOnly/MultiSendCallOnlyWeb3Contract'
+import MultiSendBaseContractWeb3 from './contracts/MultiSend/MultiSendBaseContractWeb3'
+import MultiSendCallOnlyBaseContractWeb3 from './contracts/MultiSend/MultiSendCallOnlyBaseContractWeb3'
 import SafeContractWeb3 from './contracts/Safe/SafeContractWeb3'
 import SafeProxyFactoryWeb3Contract, {
   CreateProxyProps
@@ -12,8 +12,8 @@ import { Web3TransactionOptions, Web3TransactionResult } from './types'
 export {
   CreateCallWeb3Contract,
   CreateProxyProps,
-  MultiSendCallOnlyWeb3Contract,
-  MultiSendWeb3Contract,
+  MultiSendCallOnlyBaseContractWeb3,
+  MultiSendBaseContractWeb3,
   SafeContractWeb3,
   SafeProxyFactoryWeb3Contract,
   SignMessageLibWeb3Contract,

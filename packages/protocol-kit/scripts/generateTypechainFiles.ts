e06a4575e3a7b05e41b7723d0332f90f2166c308
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2024-02-01 15:12:31 +0100

    Remove commented lines

--- packages/protocol-kit/scripts/generateTypechainFiles.ts
+++ packages/protocol-kit/scripts/generateTypechainFiles.ts
@@ -16,23 +16,16 @@ const outDirTests = 'typechain/tests/'
 const safeContractsPath = '../../node_modules/@safe-global/safe-deployments/dist/assets'
 
 const safeContracts_V1_4_1 = [
-  // `${safeContractsPath}/v1.4.1/safe.json`, // Remove contract 1.4.1 from typechain as it's migrated to Abitype
-  // `${safeContractsPath}/v1.4.1/safe_proxy_factory.json`, // Remove contract 1.4.1 from typechain as it's migrated to Abitype
   `${safeContractsPath}/v1.4.1/compatibility_fallback_handler.json`,
   `${safeContractsPath}/v1.4.1/create_call.json`,
   `${safeContractsPath}/v1.4.1/simulate_tx_accessor.json`
 ].join(' ')
 const safeContracts_V1_3_0 = [
-  // `${safeContractsPath}/v1.3.0/gnosis_safe.json`, // Remove contract 1.3.0 from typechain as it's migrated to Abitype
-  // `${safeContractsPath}/v1.3.0/proxy_factory.json`, // Remove contract 1.3.0 from typechain as it's migrated to Abitype
   `${safeContractsPath}/v1.3.0/compatibility_fallback_handler.json`,
   `${safeContractsPath}/v1.3.0/create_call.json`,
   `${safeContractsPath}/v1.3.0/simulate_tx_accessor.json`
 ].join(' ')
-const safeContracts_V1_0_0 = [
-  `${safeContractsPath}/v1.0.0/gnosis_safe.json`
-  // `${safeContractsPath}/v1.0.0/proxy_factory.json` // Remove contract 1.0.0 from typechain as it's migrated to Abitype
-].join(' ')
+const safeContracts_V1_0_0 = [`${safeContractsPath}/v1.0.0/gnosis_safe.json`].join(' ')
 
 // Won't be included in dist/ folder
 const safeContractsTestV1_4_1Path =

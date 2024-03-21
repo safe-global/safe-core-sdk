602682132f73011ecaaa59633eec77742a2e31dc
Yago Pérez Vázquez <yago@safe.global>
2024-03-21 15:30:35 +0100

    Refactor

--- packages/relay-kit/src/packs/safe-4337/Safe4337Pack.test.ts
+++ packages/relay-kit/src/packs/safe-4337/Safe4337Pack.test.ts
@@ -6,79 +6,50 @@ import {
   getAddModulesLibDeployment,
   getSafe4337ModuleDeployment
 } from '@safe-global/safe-modules-deployments'
-import { Safe4337InitOptions } from './types'
 import * as constants from './constants'
 import SafeOperation from './SafeOperation'
 import { MetaTransactionData } from '@safe-global/safe-core-sdk-types'
+import * as fixtures from './testing-utils/fixtures'
+import { createSafe4337Pack, generateTransferCallData } from './testing-utils/helpers'
 
 dotenv.config()
-
-let ethersAdapter: protocolKit.EthersAdapter
-const RPC_URL = 'https://rpc.ankr.com/eth_sepolia'
-const BUNDLER_URL = 'https://rpc.ankr.com/eth_sepolia'
-const OWNER_1 = '0xFfAC5578BE8AC1B2B9D13b34cAf4A074B96B8A1b'
-const SAFE_ADDRESS_v1_4_1 = '0x717f4BB83D8DF2e5a3Cc603Ee27263ac9EFB6c12'
-const SAFE_ADDRESS_v1_3_0 = '0x8C35a08Af278518B59D04ddDe3F1b370aD766D22'
-const PAYMASTER_ADDRESS = '0x0000000000325602a77416A16136FDafd04b299f'
-const PAYMASTER_TOKEN_ADDRESS = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238'
-const CHAIN_ID = '0xaa36a7'
 
 jest.mock('./utils', () => ({
   ...jest.requireActual('./utils'),
   getEip4337BundlerProvider: () => ({
     send: async (method: string) => {
-      if (method === 'eth_supportedEntryPoints') {
-        return [
-          '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
-          '0x0000000071727De22E5E9d8BAf0edAc6f37da032'
-        ]
-      }
+      switch (method) {
+        case constants.RPC_4337_CALLS.SUPPORTED_ENTRY_POINTS:
+          return fixtures.ENTRYPOINTS
 
-      if (method === 'eth_chainId') {
-        return CHAIN_ID
-      }
+        case constants.RPC_4337_CALLS.CHAIN_ID:
+          return fixtures.CHAIN_ID
 
-      return undefined
+        case constants.RPC_4337_CALLS.SEND_USER_OPERATION:
+          return fixtures.USER_OPERATION_HASH
+
+        case constants.RPC_4337_CALLS.ESTIMATE_USER_OPERATION_GAS:
+          return fixtures.GAS_ESTIMATION
+
+        case constants.RPC_4337_CALLS.GET_USER_OPERATION_BY_HASH:
+          return fixtures.USER_OPERATION
+
+        case constants.RPC_4337_CALLS.GET_USER_OPERATION_RECEIPT:
+          return fixtures.USER_OPERATION_RECEIPT
+
+        default:
+          return undefined
+      }
     }
   })
 }))
-
-const generateTransferCallData = (to: string, value: bigint) => {
-  const functionAbi = 'function transfer(address _to, uint256 _value) returns (bool)'
-  const iface = new ethers.Interface([functionAbi])
-
-  return iface.encodeFunctionData('transfer', [to, value])
-}
-
-const createSafe4337Pack = async (
-  initOptions: Partial<Safe4337InitOptions>
-): Promise<Safe4337Pack> => {
-  const provider = new ethers.JsonRpcProvider(RPC_URL)
-  const signer = new ethers.Wallet(process.env.PRIVATE_KEY || '0x', provider)
-  ethersAdapter = new protocolKit.EthersAdapter({
-    ethers,
-    signerOrProvider: signer
-  })
-
-  const safe4337Pack = await Safe4337Pack.init({
-    options: {
-      safeAddress: ''
-    },
-    ...initOptions,
-    ethersAdapter,
-    rpcUrl: RPC_URL,
-    bundlerUrl: BUNDLER_URL
-  })
-
-  return safe4337Pack
-}
 
 let safe4337ModuleAddress: string
 let addModulesLibAddress: string
 
 describe('Safe4337Pack', () => {
   beforeAll(async () => {
-    const network = parseInt(CHAIN_ID).toString()
+    const network = parseInt(fixtures.CHAIN_ID).toString()
     safe4337ModuleAddress = getSafe4337ModuleDeployment({
       released: true,
       version: '0.2.0',
@@ -97,7 +68,7 @@ describe('Safe4337Pack', () => {
   describe('When using existing Safe Accounts with version lesser than 1.4.1', () => {
     it('should throw an error if the Safe account version is not greater than 1.4.1', async () => {
       await expect(
-        createSafe4337Pack({ options: { safeAddress: SAFE_ADDRESS_v1_3_0 } })
+        createSafe4337Pack({ options: { safeAddress: fixtures.SAFE_ADDRESS_v1_3_0 } })
       ).rejects.toThrow(
         'Incompatibility detected: The current Safe Account version (1.3.0) is not supported. EIP-4337 requires the Safe to use at least v1.4.1.'
       )
@@ -107,18 +78,18 @@ describe('Safe4337Pack', () => {
   describe('When using existing Safe Accounts with version 1.4.1 or greater', () => {
     it('should be able to instantiate the pack using a existing Safe', async () => {
       const safe4337Pack = await createSafe4337Pack({
-        options: { safeAddress: SAFE_ADDRESS_v1_4_1 }
+        options: { safeAddress: fixtures.SAFE_ADDRESS_v1_4_1 }
       })
 
       expect(safe4337Pack).toBeInstanceOf(Safe4337Pack)
       expect(safe4337Pack.protocolKit).toBeInstanceOf(Safe)
-      expect(await safe4337Pack.protocolKit.getAddress()).toBe(SAFE_ADDRESS_v1_4_1)
-      expect(await safe4337Pack.getChainId()).toBe(CHAIN_ID)
+      expect(await safe4337Pack.protocolKit.getAddress()).toBe(fixtures.SAFE_ADDRESS_v1_4_1)
+      expect(await safe4337Pack.getChainId()).toBe(fixtures.CHAIN_ID)
     })
 
     it('should have the 4337 module enabled', async () => {
       const safe4337Pack = await createSafe4337Pack({
-        options: { safeAddress: SAFE_ADDRESS_v1_4_1 }
+        options: { safeAddress: fixtures.SAFE_ADDRESS_v1_4_1 }
       })
 
       expect(await safe4337Pack.protocolKit.getModules()).toEqual([safe4337ModuleAddress])
@@ -131,7 +102,7 @@ describe('Safe4337Pack', () => {
             safe4337ModuleAddress: '0xCustomModule'
           },
           options: {
-            safeAddress: SAFE_ADDRESS_v1_4_1
+            safeAddress: fixtures.SAFE_ADDRESS_v1_4_1
           }
         })
       ).rejects.toThrow(
@@ -142,7 +113,7 @@ describe('Safe4337Pack', () => {
     it('should use the 4337 module as the fallback handler', async () => {
       const safe4337Pack = await createSafe4337Pack({
         options: {
-          safeAddress: SAFE_ADDRESS_v1_4_1
+          safeAddress: fixtures.SAFE_ADDRESS_v1_4_1
         }
       })
 
@@ -154,7 +125,7 @@ describe('Safe4337Pack', () => {
     it('should be able to instantiate the pack using a predicted Safe', async () => {
       const safe4337Pack = await createSafe4337Pack({
         options: {
-          owners: [OWNER_1],
+          owners: [fixtures.OWNER_1],
           threshold: 1
         }
       })
@@ -178,7 +149,7 @@ describe('Safe4337Pack', () => {
         createSafe4337Pack({
           // @ts-expect-error - An error will be thrown
           options: {
-            owners: [OWNER_1]
+            owners: [fixtures.OWNER_1]
           }
         })
       ).rejects.toThrow('Owners and threshold are required to deploy a new Safe')
@@ -188,23 +159,23 @@ describe('Safe4337Pack', () => {
       const encodeFunctionDataSpy = jest.spyOn(constants.INTERFACES, 'encodeFunctionData')
       const safeCreateSpy = jest.spyOn(Safe, 'create')
 
-      await createSafe4337Pack({
+      const safe4337Pack = await createSafe4337Pack({
         options: {
-          owners: [OWNER_1],
+          owners: [fixtures.OWNER_1],
           threshold: 1
         }
       })
 
       expect(encodeFunctionDataSpy).toHaveBeenCalledWith('enableModules', [[safe4337ModuleAddress]])
       expect(safeCreateSpy).toHaveBeenCalledWith({
-        ethAdapter: ethersAdapter,
+        ethAdapter: safe4337Pack.protocolKit.getEthAdapter(),
         predictedSafe: {
           safeDeploymentConfig: {
             safeVersion: constants.DEFAULT_SAFE_VERSION,
             saltNonce: undefined
           },
           safeAccountConfig: {
-            owners: [OWNER_1],
+            owners: [fixtures.OWNER_1],
             threshold: 1,
             to: addModulesLibAddress,
             data: constants.INTERFACES.encodeFunctionData('enableModules', [
@@ -225,12 +196,12 @@ describe('Safe4337Pack', () => {
 
       const safe4337Pack = await createSafe4337Pack({
         options: {
-          owners: [OWNER_1],
+          owners: [fixtures.OWNER_1],
           threshold: 1
         },
         paymasterOptions: {
-          paymasterAddress: PAYMASTER_ADDRESS,
-          paymasterTokenAddress: PAYMASTER_TOKEN_ADDRESS
+          paymasterAddress: fixtures.PAYMASTER_ADDRESS,
+          paymasterTokenAddress: fixtures.PAYMASTER_TOKEN_ADDRESS
         }
       })
 
@@ -238,25 +209,25 @@ describe('Safe4337Pack', () => {
         [safe4337ModuleAddress]
       ])
       const approveData = constants.INTERFACES.encodeFunctionData('approve', [
-        PAYMASTER_ADDRESS,
+        fixtures.PAYMASTER_ADDRESS,
         0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffn
       ])
 
       const multiSendData = protocolKit.encodeMultiSendData([
         { to: addModulesLibAddress, value: '0', data: enableModulesData, operation: 1 },
-        { to: PAYMASTER_TOKEN_ADDRESS, value: '0', data: approveData, operation: 0 }
+        { to: fixtures.PAYMASTER_TOKEN_ADDRESS, value: '0', data: approveData, operation: 0 }
       ])
 
       expect(encodeFunctionDataSpy).toHaveBeenNthCalledWith(4, 'multiSend', [multiSendData])
       expect(safeCreateSpy).toHaveBeenCalledWith({
-        ethAdapter: ethersAdapter,
+        ethAdapter: safe4337Pack.protocolKit.getEthAdapter(),
         predictedSafe: {
           safeDeploymentConfig: {
             safeVersion: constants.DEFAULT_SAFE_VERSION,
             saltNonce: undefined
           },
           safeAccountConfig: {
-            owners: [OWNER_1],
+            owners: [fixtures.OWNER_1],
             threshold: 1,
             to: await safe4337Pack.protocolKit.getMultiSendAddress(),
             data: constants.INTERFACES.encodeFunctionData('multiSend', [multiSendData]),
@@ -277,13 +248,13 @@ describe('Safe4337Pack', () => {
     beforeAll(async () => {
       safe4337Pack = await createSafe4337Pack({
         options: {
-          safeAddress: SAFE_ADDRESS_v1_4_1
+          safeAddress: fixtures.SAFE_ADDRESS_v1_4_1
         }
       })
 
       transferUSDC = {
-        to: PAYMASTER_TOKEN_ADDRESS,
-        data: generateTransferCallData(SAFE_ADDRESS_v1_4_1, 100_000n),
+        to: fixtures.PAYMASTER_TOKEN_ADDRESS,
+        data: generateTransferCallData(fixtures.SAFE_ADDRESS_v1_4_1, 100_000n),
         value: '0',
         operation: 0
       }
@@ -298,7 +269,7 @@ describe('Safe4337Pack', () => {
 
       expect(safeOperation).toBeInstanceOf(SafeOperation)
       expect(safeOperation.data).toMatchObject({
-        safe: SAFE_ADDRESS_v1_4_1,
+        safe: fixtures.SAFE_ADDRESS_v1_4_1,
         entryPoint: '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
         initCode: '0x',
         paymasterAndData: '0x',
@@ -328,7 +299,7 @@ describe('Safe4337Pack', () => {
 
       expect(safeOperation).toBeInstanceOf(SafeOperation)
       expect(safeOperation.data).toMatchObject({
-        safe: SAFE_ADDRESS_v1_4_1,
+        safe: fixtures.SAFE_ADDRESS_v1_4_1,
         entryPoint: '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
         initCode: '0x',
         paymasterAndData: '0x',
@@ -352,7 +323,7 @@ describe('Safe4337Pack', () => {
     it('should fill the initCode property when the Safe does not exist', async () => {
       const safe4337Pack = await createSafe4337Pack({
         options: {
-          owners: [OWNER_1],
+          owners: [fixtures.OWNER_1],
           threshold: 1
         }
       })
@@ -372,15 +343,15 @@ describe('Safe4337Pack', () => {
 
   it('should all to sign a SafeOperation', async () => {
     const transferUSDC = {
-      to: PAYMASTER_TOKEN_ADDRESS,
-      data: generateTransferCallData(SAFE_ADDRESS_v1_4_1, 100_000n),
+      to: fixtures.PAYMASTER_TOKEN_ADDRESS,
+      data: generateTransferCallData(fixtures.SAFE_ADDRESS_v1_4_1, 100_000n),
       value: '0',
       operation: 0
     }
 
     const safe4337Pack = await createSafe4337Pack({
       options: {
-        safeAddress: SAFE_ADDRESS_v1_4_1
+        safeAddress: fixtures.SAFE_ADDRESS_v1_4_1
       }
     })
 
@@ -390,13 +361,39 @@ describe('Safe4337Pack', () => {
 
     expect(await safe4337Pack.signSafeOperation(safeOperation)).toMatchObject({
       signatures: new Map().set(
-        OWNER_1.toLowerCase(),
+        fixtures.OWNER_1.toLowerCase(),
         new protocolKit.EthSafeSignature(
-          OWNER_1,
+          fixtures.OWNER_1,
           '0x1199e9efb83d194d6a94e6b3d2a392fb1f072c68e83092bbf2fa15e7632b3af836791575f96be6f5890220e6386a30c05c8d6597919942c014ef3cc87e165f061c',
           false
         )
       )
     })
+  })
+
+  it('should allow to send an UserOperation to a bundler', async () => {
+    const transferUSDC = {
+      to: fixtures.PAYMASTER_TOKEN_ADDRESS,
+      data: generateTransferCallData(fixtures.SAFE_ADDRESS_v1_4_1, 100_000n),
+      value: '0',
+      operation: 0
+    }
+
+    const safe4337Pack = await createSafe4337Pack({
+      options: {
+        safeAddress: fixtures.SAFE_ADDRESS_v1_4_1
+      }
+    })
+
+    let safeOperation = await safe4337Pack.createTransaction({
+      transactions: [transferUSDC]
+    })
+    safeOperation = await safe4337Pack.signSafeOperation(safeOperation)
+
+    const sendUserOperationSpy = jest.spyOn(safe4337Pack, 'sendUserOperation')
+
+    await safe4337Pack.executeTransaction({ executable: safeOperation })
+
+    expect(sendUserOperationSpy).toHaveBeenCalledWith(safeOperation.toUserOperation())
   })
 })

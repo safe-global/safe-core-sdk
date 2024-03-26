aabbd3b0f8df7932a71b45c54f8414925a2849b2
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2024-03-26 14:23:49 +0000

    Create adapter-specific base classes for SimulateTxAccessor contract

---
+++ packages/protocol-kit/src/adapters/ethers/contracts/SimulateTxAccessor/SimulateTxAccessorBaseContractEthers.ts
@@ -0,0 +1,59 @@
+import { Contract, ContractRunner, InterfaceAbi } from 'ethers'
+
+import EthersAdapter from '@safe-global/protocol-kit/adapters/ethers/EthersAdapter'
+import SimulateTxAccessorBaseContract from '@safe-global/protocol-kit/adapters/SimulateTxAccessorBaseContract'
+import { SafeVersion } from '@safe-global/safe-core-sdk-types'
+
+/**
+ * Abstract class SimulateTxAccessorBaseContractEthers extends SimulateTxAccessorBaseContract to specifically integrate with the Ethers.js v6 library.
+ * It is designed to be instantiated for different versions of the Safe contract.
+ *
+ * This abstract class sets up the Ethers v6 Contract object that interacts with a SimulateTxAccessor contract version.
+ *
+ * Subclasses of SimulateTxAccessorBaseContractEthers are expected to represent specific versions of the contract.
+ *
+ * @template SimulateTxAccessorContractAbiType - The ABI type specific to the version of the SimulateTxAccessor contract, extending InterfaceAbi from Ethers.
+ * @extends SimulateTxAccessorBaseContract<SimulateTxAccessorContractAbiType> - Extends the generic SimulateTxAccessorBaseContract with Ethers-specific implementation.
+ *
+ * Example subclasses:
+ * - SimulateTxAccessorContract_v1_4_1_Ethers extends SimulateTxAccessorBaseContractEthers<SimulateTxAccessorContract_v1_4_1_Abi>
+ * - SimulateTxAccessorContract_v1_3_0_Ethers extends SimulateTxAccessorBaseContractEthers<SimulateTxAccessorContract_v1_3_0_Abi>
+ */
+abstract class SimulateTxAccessorBaseContractEthers<
+  SimulateTxAccessorContractAbiType extends InterfaceAbi
+> extends SimulateTxAccessorBaseContract<SimulateTxAccessorContractAbiType> {
+  contract: Contract
+  adapter: EthersAdapter
+
+  /**
+   * @constructor
+   * Constructs an instance of SimulateTxAccessorBaseContractEthers.
+   *
+   * @param chainId - The chain ID of the contract.
+   * @param ethersAdapter - An instance of EthersAdapter.
+   * @param defaultAbi - The default ABI for the SimulateTxAccessor contract. It should be compatible with the specific version of the contract.
+   * @param safeVersion - The version of the Safe contract.
+   * @param customContractAddress - Optional custom address for the contract. If not provided, the address is derived from the Safe deployments based on the chainId and safeVersion.
+   * @param customContractAbi - Optional custom ABI for the contract. If not provided, the ABI is derived from the Safe deployments or the defaultAbi is used.
+   */
+  constructor(
+    chainId: bigint,
+    ethersAdapter: EthersAdapter,
+    defaultAbi: SimulateTxAccessorContractAbiType,
+    safeVersion: SafeVersion,
+    customContractAddress?: string,
+    customContractAbi?: SimulateTxAccessorContractAbiType,
+    runner?: ContractRunner | null
+  ) {
+    super(chainId, defaultAbi, safeVersion, customContractAddress, customContractAbi)
+
+    this.adapter = ethersAdapter
+    this.contract = new Contract(
+      this.contractAddress,
+      this.contractAbi,
+      runner || this.adapter.getSigner()
+    )
+  }
+}
+
+export default SimulateTxAccessorBaseContractEthers

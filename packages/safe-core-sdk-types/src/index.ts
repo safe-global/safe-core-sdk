ffa1275bc5b24da13593ed35d2e043ce78988d3d
Daniel Somoza <daniel.somoza@safe.global>
2024-04-16 16:42:02 +0200

    removed abi.d.ts file

--- packages/safe-core-sdk-types/src/index.ts
+++ packages/safe-core-sdk-types/src/index.ts
@@ -10,3 +10,19 @@ export * from './contracts/assets'
 export * from './ethereumLibs/ethers/types'
 export * from './ethereumLibs/web3/types'
 export * from './types'
+
+// see docs: https://abitype.dev/config
+declare module 'abitype' {
+  export interface Register {
+    // AddressType: `0x${string}`
+    // BytesType: {
+    //   inputs: `0x${string}` | Uint8Array
+    //   outputs: `0x${string}`
+    // }
+    AddressType: string
+    BytesType: {
+      inputs: string
+      outputs: string
+    }
+  }
+}

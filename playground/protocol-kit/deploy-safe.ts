a6ef23c6e619f0ee67d848f5b1780a17cadab2d3
Yago Pérez Vázquez <yago@safe.global>
2024-04-25 15:17:54 +0200

    Remove imports

--- playground/protocol-kit/deploy-safe.ts
+++ playground/protocol-kit/deploy-safe.ts
@@ -1,6 +1,5 @@
 import { SafeAccountConfig, SafeFactory } from '@safe-global/protocol-kit'
 import { SafeVersion } from '@safe-global/safe-core-sdk-types'
-import { ethers } from 'ethers'
 
 // This file can be used to play around with the Safe Core SDK
 
--- playground/protocol-kit/eip1271.ts
+++ playground/protocol-kit/eip1271.ts
@@ -1,6 +1,5 @@
 import Safe from '@safe-global/protocol-kit'
 import { hashSafeMessage } from '@safe-global/protocol-kit'
-import { ethers, JsonRpcProvider } from 'ethers'
 
 // This file can be used to play around with the Safe Core SDK
 

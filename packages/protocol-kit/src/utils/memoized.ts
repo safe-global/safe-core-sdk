783abf4dbfbc60bbf08eea22f53e082d590038a7
Yago Pérez Vázquez <yago@safe.global>
2024-04-30 09:45:55 +0200

    Add memoize again

--- packages/protocol-kit/src/utils/memoized.ts
+++ packages/protocol-kit/src/utils/memoized.ts
@@ -3,10 +3,30 @@ export function createMemoizedFunction<T extends (...args: Parameters<T>) => Ret
   cache: Record<string, ReturnType<T>> = {}
 ) {
   return (...args: Parameters<T>): ReturnType<T> => {
-    const key = JSON.stringify(args)
+    const key = JSON.stringify(args, bigIntSerializerReplacer)
 
     cache[key] = cache[key] || callback(...args)
 
     return cache[key]
+  }
+}
+
+// EIP1193 providers from web3.currentProvider and hre.network.provider fail to serialize BigInts
+function bigIntSerializerReplacer() {
+  const seen = new Set()
+
+  return (_: string, value: unknown) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return undefined
+      }
+      seen.add(value)
+    }
+
+    if (typeof value === 'bigint') {
+      return value.toString()
+    }
+
+    return value
   }
 }

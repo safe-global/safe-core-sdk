83f494e9b9157ba9d72f28934596634e03370fca
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2024-05-08 18:02:05 +0200

    Assert length of SafeOperations in the getSafeOperation e2e test

--- packages/api-kit/tests/e2e/getSafeOperation.test.ts
+++ packages/api-kit/tests/e2e/getSafeOperation.test.ts
@@ -30,6 +30,8 @@ describe('getSafeOperation', () => {
 
   it('should get the SafeOperation', async () => {
     const safeOperations = await safeApiKit.getSafeOperationsByAddress(SAFE_ADDRESS)
+    chai.expect(safeOperations.results.length).to.have.above(0)
+
     const safeOperationHash = safeOperations.results[0].safeOperationHash
 
     const safeOperation = await safeApiKit.getSafeOperation(safeOperationHash)

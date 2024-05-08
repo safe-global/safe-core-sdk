85d19589f64e76d85d75050eba3f80a6993f0fa2
Yago Pérez Vázquez <yago@safe.global>
2024-05-08 12:44:52 +0200

    Restore test

--- packages/api-kit/tests/e2e/getTransactionConfirmations.test.ts
+++ packages/api-kit/tests/e2e/getTransactionConfirmations.test.ts
@@ -19,6 +19,13 @@ describe('getTransactionConfirmations', () => {
       .to.be.rejectedWith('Invalid safeTxHash')
   })
 
+  it('should return an empty array if the safeTxHash is not found', async () => {
+    const safeTxHash = '0x317834aea988fd3cfa54fd8b2be2c96b4fd70a14d8c9470a7110576b01e6480b'
+    const transactionConfirmations = await safeApiKit.getTransactionConfirmations(safeTxHash)
+    chai.expect(transactionConfirmations.count).to.be.equal(0)
+    chai.expect(transactionConfirmations.results.length).to.be.equal(0)
+  })
+
   it('should return the transaction with the given safeTxHash', async () => {
     const safeTxHash = '0x317834aea988fd3cfa54fd8b2be2c96b4fd70a14d8c9470a7110576b01e6480a'
     const transactionConfirmations = await safeApiKit.getTransactionConfirmations(safeTxHash)

0fd6ee0486fd1d417332ec112fdea48c72d05231
Daniel <25051234+dasanra@users.noreply.github.com>
2024-09-13 16:43:37 +0200

    chore: remove unused function

--- packages/protocol-kit/tests/e2e/utils/transactions.ts
+++ packages/protocol-kit/tests/e2e/utils/transactions.ts
@@ -1,6 +1,5 @@
 import { GetTransactionReceiptReturnType } from 'viem'
 import { TransactionResult } from '@safe-global/safe-core-sdk-types'
-import SafeProvider from '@safe-global/protocol-kit/SafeProvider'
 
 export async function waitSafeTxReceipt(
   txResult: TransactionResult
@@ -9,11 +8,4 @@ export async function waitSafeTxReceipt(
     txResult.transactionResponse && (await txResult?.transactionResponse?.wait())
 
   return receipt
-}
-
-export async function getTransaction(
-  safeProvider: SafeProvider,
-  transactionHash: string
-): Promise<any> {
-  return safeProvider.getTransaction(transactionHash)
 }

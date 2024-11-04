3dd4f2dd300ef7a49ef88c372cb6a90f66bb1ad0
Leoni Mella <leoni.mella@gmail.com>
2024-11-04 10:38:09 -0300

    feat: add shortname for ink sepolia

--- packages/protocol-kit/src/utils/eip-3770/config.ts
+++ packages/protocol-kit/src/utils/eip-3770/config.ts
@@ -271,6 +271,7 @@ export const networks: NetworkShortName[] = [
   { chainId: 656476n, shortName: 'open-campus-codex' },
   { chainId: 660279n, shortName: 'xai' },
   { chainId: 713715n, shortName: 'sei-devnet' },
+  { chainId: 763373n, shortName: 'ink-sepolia' },
   { chainId: 764984n, shortName: 'lamina1test' },
   { chainId: 808813n, shortName: 'bob-sepolia' },
   { chainId: 810180n, shortName: 'zklink-nova' },

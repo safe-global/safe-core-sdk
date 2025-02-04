9601e11c65bef36ebaca9f3e3e940050b7bef901
Yago Pérez Vázquez <yago@safe.global>
2025-02-04 12:34:26 +0100

    Improve SafeOperationFactory

--- packages/relay-kit/src/packs/safe-4337/SafeOperationFactory.ts
+++ packages/relay-kit/src/packs/safe-4337/SafeOperationFactory.ts
@@ -7,18 +7,24 @@ import {
 import SafeOperationV06 from '@safe-global/relay-kit/packs/safe-4337/SafeOperationV06'
 import SafeOperationV07 from '@safe-global/relay-kit/packs/safe-4337/SafeOperationV07'
 import SafeOperationBase from '@safe-global/relay-kit/packs/safe-4337/SafeOperationBase'
-import { isEntryPointV7 } from '@safe-global/relay-kit/packs/safe-4337/utils'
+import { isEntryPointV6 } from '@safe-global/relay-kit/packs/safe-4337/utils'
 
 class SafeOperationFactory {
+  /**
+   * Creates a new SafeOperation with proper validation
+   * @param userOperation - The base user operation
+   * @param options - Configuration options
+   * @returns Validated SafeOperation instance
+   */
   static createSafeOperation(
     userOperation: UserOperation,
     options: SafeOperationOptions
   ): SafeOperationBase {
-    if (isEntryPointV7(options.entryPoint)) {
-      return new SafeOperationV07(userOperation as UserOperationV07, options)
-    } else {
+    if (isEntryPointV6(options.entryPoint)) {
       return new SafeOperationV06(userOperation as UserOperationV06, options)
     }
+
+    return new SafeOperationV07(userOperation as UserOperationV07, options)
   }
 }
 

44e361e3266370db464e14fdb51b0ef1cc2477e5
Yago Pérez Vázquez <yago@safe.global>
2025-05-19 16:08:03 +0200

    use new fn

--- packages/api-kit/tests/endpoint/index.test.ts
+++ packages/api-kit/tests/endpoint/index.test.ts
@@ -17,6 +17,7 @@ import { UserOperation } from '@safe-global/types-kit'
 import { signDelegate } from '@safe-global/api-kit/utils/signDelegate'
 import config from '../utils/config'
 import { getApiKit, getKits } from '../utils/setupKits'
+import { getTransactionServiceUrl } from '@safe-global/api-kit/utils/config'
 
 chai.use(chaiAsPromised)
 chai.use(sinonChai)
@@ -34,8 +35,7 @@ const tokenAddress = '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14'
 const eip3770TokenAddress = `${config.EIP_3770_PREFIX}:${tokenAddress}`
 const safeTxHash = '0x317834aea988fd3cfa54fd8b2be2c96b4fd70a14d8c9470a7110576b01e6480a'
 const safeOpHash = '0x8b1840745ec0a6288e868c6e285dadcfebd49e846d307610a9ccd97f445ace93'
-//TODO: Update with the production one
-const txServiceBaseUrl = 'https://api.5afe.dev/tx-service/11155111/api'
+const txServiceBaseUrl = getTransactionServiceUrl(11155111n)
 
 const signer = createWalletClient({
   transport: http(),

63488df97d9f03010ff23b4d9ef6d24f040b4575
Yago Pérez Vázquez <yago@safe.global>
2025-06-30 10:24:00 +0200

    export PimlicoFeeEstimator

--- packages/relay-kit/src/index.ts
+++ packages/relay-kit/src/index.ts
@@ -16,6 +16,7 @@ export * from './packs/safe-4337/utils'
 export * from './RelayKitBasePack'
 
 export { GenericFeeEstimator } from './packs/safe-4337/estimators/generic/GenericFeeEstimator'
+export { PimlicoFeeEstimator } from './packs/safe-4337/estimators/pimlico/PimlicoFeeEstimator'
 
 declare module 'abitype' {
   export interface Register {

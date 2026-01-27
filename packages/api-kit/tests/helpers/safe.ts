943e052c1296077ec2e93cad133e3f471801775d
Akshay Patel <akshay.ap95@gmail.com>
2026-01-27 10:58:16 +0100

    Use v1.4.1 contracts for tests

--- packages/api-kit/tests/helpers/safe.ts
+++ packages/api-kit/tests/helpers/safe.ts
@@ -1,15 +1,15 @@
 export const API_TESTING_SAFE = {
-  address: '0xF8ef84392f7542576F6b9d1b140334144930Ac78',
-  nonce: '10',
+  address: '0x4103eEB76EBBcB652F8B15d3817EBFC07b664b0c',
+  nonce: '0',
   threshold: 2,
   owners: [
     '0x56e2C102c664De6DfD7315d12c0178b61D16F171',
     '0x9cCBDE03eDd71074ea9c49e413FA9CDfF16D263B',
-    '0xDa8dd250065F19f7A29564396D7F13230b9fC5A3'
+    '0xDa734d0A0c2328394e3DbC88cDcD4D197142C37D'
   ],
   modules: [],
-  fallbackHandler: '0x017062a1dE2FE6b99BE3d9d37841FeD19F573804',
+  fallbackHandler: '0xfd0732dc9e303f09fcef3a7388ad10a83459ec99',
   guard: '0x0000000000000000000000000000000000000000',
-  version: '1.3.0+L2',
-  singleton: '0xfb1bffC9d739B8D520DaF37dF666da4C687191EA'
+  version: '1.4.1+L2',
+  singleton: '0x29fcb43b46531bca003ddc8fcb67ffe91900c762'
 }

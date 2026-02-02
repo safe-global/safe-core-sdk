726194c275f02e4254bb870327cf3a8083aecca6
Akshay Patel <akshay.ap95@gmail.com>
2026-02-02 14:18:11 +0100

    Fix: Add API_TESTING_SAFE_1_4_1 in safe.ts for e2e tests

--- packages/api-kit/tests/helpers/safe.ts
+++ packages/api-kit/tests/helpers/safe.ts
@@ -1,4 +1,20 @@
 export const API_TESTING_SAFE = {
+  address: '0xF8ef84392f7542576F6b9d1b140334144930Ac78',
+  nonce: '10',
+  threshold: 2,
+  owners: [
+    '0x56e2C102c664De6DfD7315d12c0178b61D16F171',
+    '0x9cCBDE03eDd71074ea9c49e413FA9CDfF16D263B',
+    '0xDa8dd250065F19f7A29564396D7F13230b9fC5A3'
+  ],
+  modules: [],
+  fallbackHandler: '0x017062a1dE2FE6b99BE3d9d37841FeD19F573804',
+  guard: '0x0000000000000000000000000000000000000000',
+  version: '1.3.0+L2',
+  singleton: '0xfb1bffC9d739B8D520DaF37dF666da4C687191EA'
+}
+
+export const API_TESTING_SAFE_1_4_1 = {
   address: '0x4103eEB76EBBcB652F8B15d3817EBFC07b664b0c',
   nonce: '0',
   threshold: 2,

8514dcf79cf65cacb7bb1a30d414bb42477587d0
Akshay Patel <akshay.ap95@gmail.com>
2026-02-27 12:55:36 +0100

    Feat: Add support for SafeContract v1.5.0 and update SAFE_FEATURES enum

--- packages/protocol-kit/src/utils/safeVersions.ts
+++ packages/protocol-kit/src/utils/safeVersions.ts
@@ -5,6 +5,7 @@ import SafeContract_v1_1_1 from '@safe-global/protocol-kit/contracts/Safe/v1.1.1
 import SafeContract_v1_2_0 from '@safe-global/protocol-kit/contracts/Safe/v1.2.0/SafeContract_v1_2_0'
 import SafeContract_v1_3_0 from '@safe-global/protocol-kit/contracts/Safe/v1.3.0/SafeContract_v1_3_0'
 import SafeContract_v1_4_1 from '@safe-global/protocol-kit/contracts/Safe/v1.4.1/SafeContract_v1_4_1'
+import SafeContract_v1_5_0 from '@safe-global/protocol-kit/contracts/Safe/v1.5.0/SafeContract_v1_5_0'
 
 export enum SAFE_FEATURES {
   SAFE_TX_GAS_OPTIONAL = 'SAFE_TX_GAS_OPTIONAL',
@@ -15,7 +16,8 @@ export enum SAFE_FEATURES {
   REQUIRED_TXGAS = 'REQUIRED_TXGAS',
   SIMULATE_AND_REVERT = 'SIMULATE_AND_REVERT',
   PASSKEY_SIGNER = 'PASSKEY_SIGNER',
-  SAFE_L2_CONTRACTS = 'SAFE_L2_CONTRACTS'
+  SAFE_L2_CONTRACTS = 'SAFE_L2_CONTRACTS',
+  SAFE_MODULE_GUARD = 'SAFE_MODULE_GUARD'
 }
 
 const SAFE_FEATURES_BY_VERSION: Record<SAFE_FEATURES, string> = {
@@ -27,7 +29,8 @@ const SAFE_FEATURES_BY_VERSION: Record<SAFE_FEATURES, string> = {
   [SAFE_FEATURES.REQUIRED_TXGAS]: '<=1.2.0',
   [SAFE_FEATURES.SIMULATE_AND_REVERT]: '>=1.3.0',
   [SAFE_FEATURES.PASSKEY_SIGNER]: '>=1.3.0',
-  [SAFE_FEATURES.SAFE_L2_CONTRACTS]: '>=1.3.0'
+  [SAFE_FEATURES.SAFE_L2_CONTRACTS]: '>=1.3.0',
+  [SAFE_FEATURES.SAFE_MODULE_GUARD]: '>=1.5.0'
 }
 
 export const hasSafeFeature = (feature: SAFE_FEATURES, version: string): boolean => {
@@ -43,17 +46,27 @@ export type SafeContractCompatibleWithFallbackHandler =
   | SafeContract_v1_2_0
   | SafeContract_v1_3_0
   | SafeContract_v1_4_1
+  | SafeContract_v1_5_0
 
-export type SafeContractCompatibleWithGuardManager = SafeContract_v1_3_0 | SafeContract_v1_4_1
+export type SafeContractCompatibleWithGuardManager =
+  | SafeContract_v1_3_0
+  | SafeContract_v1_4_1
+  | SafeContract_v1_5_0
 
-export type SafeContractCompatibleWithModuleManager = SafeContract_v1_3_0 | SafeContract_v1_4_1
+export type SafeContractCompatibleWithModuleManager =
+  | SafeContract_v1_3_0
+  | SafeContract_v1_4_1
+  | SafeContract_v1_5_0
 
 export type SafeContractCompatibleWithRequiredTxGas =
   | SafeContract_v1_0_0
   | SafeContract_v1_1_1
   | SafeContract_v1_2_0
 
-export type SafeContractCompatibleWithSimulateAndRevert = SafeContract_v1_3_0 | SafeContract_v1_4_1
+export type SafeContractCompatibleWithSimulateAndRevert =
+  | SafeContract_v1_3_0
+  | SafeContract_v1_4_1
+  | SafeContract_v1_5_0
 
 export async function isSafeContractCompatibleWithRequiredTxGas(
   safeContract: SafeContractImplementationType
